b9ea5c786552a8b0b05bce8c53bc156f222e236c
Caspar van Leeuwen <caspar.vanleeuwen@surf.nl>
2020-08-04 15:58:52 +0200

    Reconstruct EasyConfig names. Use EasyBuild to create an ordered list of dependencies, so we can build without robot. That way, we can add additional arguments, environment variables, etc to individual builds later on (e.g. intel compiler license, etc). Finally, loop over the EasyConfig. Currently implemented by calling eb's main on the spec files retrieved from the ordered dependency list. Current issue: build fails after the first spec file, because the temporary directory is removed.

--- eessi.py
+++ eessi.py
@@ -2,7 +2,11 @@
 import re, sys
 
 from easybuild.tools.options import set_up_configuration
-
+import easybuild.main as ebmain
+from easybuild.tools.robot import resolve_dependencies
+from easybuild.tools.modules import modules_tool
+from easybuild.framework.easyconfig.tools import det_easyconfig_paths
+from easybuild.framework.easyconfig.tools import parse_easyconfigs
 
 def error(msg):
     """Print error message and exit."""
@@ -51,7 +55,7 @@ def filter_build_list(build_list, host_attrs):
             only_attrs = build_list[name][key].get('only', [])
             if not all(host_attrs.get(x, False) for x in only_attrs):
                 continue
-            
+
             # make sure no 'except' attributes are set for current host;
             # if so, filter out the key
             except_attrs = build_list[name][key].get('except', [])
@@ -62,6 +66,13 @@ def filter_build_list(build_list, host_attrs):
 
     return filtered_build_list
 
+# Get EasyBuilds robot functionality to give us back an ordered list that we
+# can use to build without robot
+def unroll_robot(easyconfig):
+    print("Unrolling %s (WIP)" % easyconfig)
+    # unrolled result will be stored here
+    easyconfiglist = [easyconfig]
+    return easyconfiglist
 
 def main():
     eb_go, _ = set_up_configuration(args=sys.argv, silent=True)
@@ -73,10 +84,31 @@ def main():
     build_list = parse_build_list(eb_go.args[1])
     filtered_build_list = filter_build_list(build_list, host_attrs)
 
-    for name in sorted(filtered_build_list):
-        print(name)
-        for key in filtered_build_list[name]:
-            print("* %s" % key)
+    modtool = modules_tool()
 
+    ec_list = [ "{0}-{1}.eb".format(name, version)
+            for name in sorted(filtered_build_list)
+            for version in filtered_build_list[name] ]
+
+    print("EasyConfig names extracted from build list:")
+    print(ec_list)
+
+    ec_paths = det_easyconfig_paths(ec_list)
+    ecs, _ = parse_easyconfigs([(p, False) for p in ec_paths], validate=False)
+    ordered_ecs = resolve_dependencies(ecs, modtool, retain_all_deps=True)
+
+    print("Ordered list of resolved dependencies")
+    for ec in ordered_ecs:
+        print(ec['spec'])
+
+    # TODO: loop still fails after one iteration, because the first build removes the tmpdir after completing succesfully. I guess we need to re-initialize before calling main again or something?
+    print("Start building")
+    for ec in ordered_ecs:
+        print(ec['spec'])
+        # Just to see if we can call EB like this, do -D to not actually build anything for now
+        ebargs=["{}".format(ec['spec'])]
+        #print(ebargs)
+        ebmain.main(ebargs)
+    print("Building completed")
 
 main()
