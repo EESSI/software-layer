576587a8bd4a1cf9f43d226c0dcecad9336e7aec
Kenneth Hoste <kenneth.hoste@ugent.be>
2020-09-28 11:05:36 +0200

    refactor eessi_software_subdir_for_host.py so it is easier to test + add tests

--- init/eessi_software_subdir_for_host.py
+++ init/eessi_software_subdir_for_host.py
@@ -27,63 +27,80 @@ def warning(msg):
     sys.stderr.write('WARNING: ' + msg + '\n')
 
 
-if len(sys.argv) == 2:
-    eessi_prefix = sys.argv[1]
-else:
-    error("Usage: %s <prefix path>" % sys.argv[0])
+def det_host_triple():
+    """
+    Determine host triple: (<cpu_family>, <cpu_vendor>, <cpu_name>).
+    <cpu_vendor> may be None if there's no match in VENDOR_MAP.
+    """
+    host_cpu = archspec.cpu.host()
+    host_vendor = VENDOR_MAP.get(host_cpu.vendor)
+    host_cpu_family = host_cpu.family.name
+    host_cpu_name = host_cpu.name
 
-eessi_software_layer_path = os.path.join(eessi_prefix, 'software')
-if not os.path.exists(eessi_software_layer_path):
-    error("Specified prefix '%s' does not exist!" % eessi_software_layer_path)
+    return (host_cpu_family, host_vendor, host_cpu_name)
 
-# determine host CPU vendor/family/name
-host_cpu = archspec.cpu.host()
-host_vendor = VENDOR_MAP.get(host_cpu.vendor)
-host_cpu_family = host_cpu.family.name
-host_cpu_name = host_cpu.name
 
-# determine available targets in 'software' subdirectory of specified prefix (only for host CPU family/vendor)
-if host_cpu_family == X86_64:
-    paths = glob.glob(os.path.join(eessi_software_layer_path, host_cpu_family, host_vendor, '*'))
-    # also consider x86_64/generic
-    generic_path = os.path.join(eessi_software_layer_path, host_cpu_family, GENERIC)
-    if os.path.exists(generic_path):
-        paths.append(generic_path)
-else:
-    paths = glob.glob(os.path.join(eessi_software_layer_path, host_cpu_family, '*'))
+def find_best_target(eessi_prefix):
 
-if not paths:
-    error("No targets found for " + host_cpu_family)
+    eessi_software_layer_path = os.path.join(eessi_prefix, 'software')
+    if not os.path.exists(eessi_software_layer_path):
+        error('Specified prefix "%s" does not exist!' % eessi_software_layer_path)
 
-targets = [os.path.basename(p) for p in paths]
+    host_cpu_family, host_vendor, host_cpu_name = det_host_triple()
 
-# retain only targets compatible with host, and sort them
-target_uarchs = []
-for uarch in targets:
-    if uarch == GENERIC:
-        continue
-    if uarch in KNOWN_CPU_UARCHS:
-        target_uarchs.append(KNOWN_CPU_UARCHS[uarch])
+    # determine available targets in 'software' subdirectory of specified prefix (only for host CPU family/vendor)
+    if host_cpu_family == X86_64:
+        paths = glob.glob(os.path.join(eessi_software_layer_path, host_cpu_family, host_vendor, '*'))
+        # also consider x86_64/generic
+        generic_path = os.path.join(eessi_software_layer_path, host_cpu_family, GENERIC)
+        if os.path.exists(generic_path):
+            paths.append(generic_path)
     else:
-        warning("Ignoring unknown target '%s'" % uarch)
+        paths = glob.glob(os.path.join(eessi_software_layer_path, host_cpu_family, '*'))
 
-host_uarch = KNOWN_CPU_UARCHS[host_cpu_name]
-compat_target_uarchs = sorted([x for x in target_uarchs if x <= host_uarch])
+    if not paths:
+        error('No targets found for ' + host_cpu_name)
 
-if compat_target_uarchs:
-    compat_target_names = [x.name for x in compat_target_uarchs]
-    cnt = len(compat_target_uarchs)
-elif GENERIC in targets:
-    compat_target_uarchs = [GENERIC]
-else:
-    error("No targets compatible with %s found!" % host_uarch)
+    targets = [os.path.basename(p) for p in paths]
 
-# last target is best pick for current host
-selected_uarch = str(compat_target_uarchs[-1])
+    # retain only targets compatible with host, and sort them
+    target_uarchs = []
+    for uarch in targets:
+        if uarch == GENERIC:
+            continue
+        if uarch in KNOWN_CPU_UARCHS:
+            target_uarchs.append(KNOWN_CPU_UARCHS[uarch])
+        else:
+            warning('Ignoring unknown target "%s"' % uarch)
 
-if selected_uarch and selected_uarch != GENERIC:
-    parts = (host_cpu_family, host_vendor, selected_uarch)
-else:
-    parts = (host_cpu_family, selected_uarch)
+    host_uarch = KNOWN_CPU_UARCHS[host_cpu_name]
+    compat_target_uarchs = sorted([x for x in target_uarchs if x <= host_uarch])
 
-print(os.path.join(*parts))
+    if not compat_target_uarchs:
+        if GENERIC in targets:
+            compat_target_uarchs = [GENERIC]
+        else:
+            error('No targets compatible with %s found!' % host_uarch)
+
+    # last target is best pick for current host
+    selected_uarch = str(compat_target_uarchs[-1])
+
+    if selected_uarch and selected_uarch != GENERIC:
+        parts = (host_cpu_family, host_vendor, selected_uarch)
+    else:
+        parts = (host_cpu_family, selected_uarch)
+
+    return os.path.join(*parts)
+
+
+def main():
+    if len(sys.argv) == 2:
+        eessi_prefix = sys.argv[1]
+    else:
+        error('Usage: %s <prefix path>' % sys.argv[0])
+
+    print(find_best_target(eessi_prefix))
+
+
+if __name__ == '__main__':
+    main()
---
+++ init/test.py
@@ -0,0 +1,82 @@
+import os
+import pytest
+import re
+
+import eessi_software_subdir_for_host
+from eessi_software_subdir_for_host import find_best_target
+
+
+def prep_tmpdir(tmpdir, subdirs):
+    for subdir in subdirs:
+        os.makedirs(os.path.join(tmpdir, 'software', subdir), exist_ok=True)
+
+
+def test_prefix_does_not_exist(capsys, tmpdir):
+    """Test whether non-existing prefix results in error."""
+
+    with pytest.raises(SystemExit):
+        find_best_target(tmpdir)
+
+    captured = capsys.readouterr()
+    assert captured.out == ''
+    assert re.match('^ERROR: Specified prefix ".*/software" does not exist!$', captured.err)
+
+
+def test_no_targets(tmpdir, capsys):
+    """Test case where no compatible targets are found for host CPU."""
+    prep_tmpdir(tmpdir, [''])
+    with pytest.raises(SystemExit):
+        find_best_target(tmpdir)
+
+    captured = capsys.readouterr()
+    assert captured.out == ''
+    assert re.match('^ERROR: No targets found for .*', captured.err)
+
+
+def test_broadwell_host(tmpdir, capsys, monkeypatch):
+    """Test selecting of target on a Broadwell host."""
+
+    def broadwell_host_triple():
+        return ('x86_64', 'intel', 'broadwell')
+
+    monkeypatch.setattr(eessi_software_subdir_for_host, 'det_host_triple', broadwell_host_triple)
+
+    # if generic is there, that's always a match
+    prep_tmpdir(tmpdir, ['x86_64/generic'])
+    assert find_best_target(tmpdir) == 'x86_64/generic'
+
+    # targets from other CPU familues have no impact on target picked for Intel host CPU
+    prep_tmpdir(tmpdir, ['x86_64/amd/zen2', 'aarch64/graviton2', 'ppc64le/power9'])
+    assert find_best_target(tmpdir) == 'x86_64/generic'
+
+    # incompatible targets are not picked
+    prep_tmpdir(tmpdir, ['x86_64/intel/skylake', 'x86_64/intel/cascadelake'])
+    assert find_best_target(tmpdir) == 'x86_64/generic'
+
+    # compatible targets are picked if no better match is available
+    prep_tmpdir(tmpdir, ['x86_64/intel/nehalem'])
+    assert find_best_target(tmpdir) == 'x86_64/intel/nehalem'
+
+    prep_tmpdir(tmpdir, ['x86_64/intel/ivybridge'])
+    assert find_best_target(tmpdir) == 'x86_64/intel/ivybridge'
+
+    # unknown targets don't cause trouble (only warning)
+    prep_tmpdir(tmpdir, ['x86_64/intel/no_such_intel_cpu'])
+    assert find_best_target(tmpdir) == 'x86_64/intel/ivybridge'
+    captured = capsys.readouterr()
+    assert captured.out == ''
+    assert captured.err == 'WARNING: Ignoring unknown target "no_such_intel_cpu"\n'
+
+    # older targets have to no impact on best target (sandybridge < ivybridge)
+    prep_tmpdir(tmpdir, ['x86_64/intel/sandybridge'])
+    assert find_best_target(tmpdir) == 'x86_64/intel/ivybridge'
+
+    prep_tmpdir(tmpdir, ['x86_64/intel/haswell'])
+    assert find_best_target(tmpdir) == 'x86_64/intel/haswell'
+
+    expected = ['cascadelake', 'haswell', 'ivybridge', 'nehalem', 'no_such_intel_cpu', 'sandybridge', 'skylake']
+    assert sorted(os.listdir(os.path.join(tmpdir, 'software', 'x86_64', 'intel'))) == expected
+
+    # exact match, no better target than this
+    prep_tmpdir(tmpdir, ['x86_64/intel/broadwell'])
+    assert find_best_target(tmpdir) == 'x86_64/intel/broadwell'
