6d1f892f16e29c9be0a83d179d5d78c700f71361
Caspar van Leeuwen <caspar.vanleeuwen@surfsara.nl>
2021-02-03 15:40:27 +0100

    Made specific class for container test. Todo: do the same for CVMFS native test

--- tests/reframe/eessi-checks/applications/gromacs.py
+++ tests/reframe/eessi-checks/applications/gromacs.py
@@ -4,13 +4,31 @@ import reframe.utility.sanity as sn
 
 # TODO:
 
-# - Split in GromacsBase parent class and GromacsContainer child class. Implement all generic things in Base
 # - Add GromacsNative class, that doesn't run based on the container but on native CVMFS mount. Test @generoso
-# - Make multiple scale variants of the container & native tests: single node, two nodes, and more (and tag with: single, small ,large)
 
 class GromacsBase(rfm.RunOnlyRegressionTest):
-    def __init__(self, nsteps):
+    def __init__(self):
         self.valid_systems = ['*']
+
+        output_file = 'md.log'
+        energy = sn.extractsingle(r'\s+Coul\. recip\.\s+Potential\s+Kinetic En\.\s+Total Energy\s+Conserved En.\n'
+                                  r'(\s+\S+){3}\s+(?P<energy>\S+)(\s+\S+){1}\n',
+                                  output_file, 'energy', float, item=-1)
+        energy_reference = -1509290.0
+
+        self.sanity_patterns = sn.all([
+            sn.assert_found('Finished mdrun', output_file),
+            sn.assert_reference(energy, energy_reference, -0.001, 0.001)
+        ])
+
+        self.maintainers = ['casparvl']
+
+class GromacsContainerBase(GromacsBase):
+    def __init__(self, nsteps):
+
+        super().__init__()
+
+        self.tags = {'container'}
         self.valid_prog_environs = ['container']
 
         self.prerun_cmds = ['source shared_alien_cache_minimal.sh > /dev/null']
@@ -29,22 +47,8 @@ class GromacsBase(rfm.RunOnlyRegressionTest):
             'gmx_mpi mdrun -s ion_channel.tpr -maxh 0.50 -resethway -noconfout -nsteps ' + nsteps
         ]
 
-        output_file = 'md.log'
-        energy = sn.extractsingle(r'\s+Coul\. recip\.\s+Potential\s+Kinetic En\.\s+Total Energy\s+Conserved En.\n'
-                                  r'(\s+\S+){3}\s+(?P<energy>\S+)(\s+\S+){1}\n',
-                                  output_file, 'energy', float, item=-1)
-        energy_reference = -1509290.0
-
-        self.sanity_patterns = sn.all([
-            sn.assert_found('Finished mdrun', output_file),
-            sn.assert_reference(energy, energy_reference, -0.001, 0.001)
-        ])
-
-        self.maintainers = ['casparvl']
-
-
 @rfm.parameterized_test(['single'], ['small'], ['large'])
-class Gromacs(GromacsBase):
+class GromacsContainer(GromacsContainerBase):
     def __init__(self, scale):
          
         if scale == 'single':
@@ -59,4 +63,4 @@ class Gromacs(GromacsBase):
 
         self.num_tasks = 24 * self.num_nodes
         self.num_tasks_per_node = 24
-        self.tags = {scale}
+        self.tags.add(scale)
