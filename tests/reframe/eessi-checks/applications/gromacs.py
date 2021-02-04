fabf21ee0f930dd3850529b4491922e5bc3413c1
Caspar van Leeuwen <caspar.vanleeuwen@surfsara.nl>
2021-02-04 19:15:53 +0100

    Somehow I failed to get the right version of the GROMACS test in this branch... well, here it is...

--- tests/reframe/eessi-checks/applications/gromacs.py
+++ tests/reframe/eessi-checks/applications/gromacs.py
@@ -2,10 +2,6 @@ import os
 import reframe as rfm
 import reframe.utility.sanity as sn
 
-# TODO:
-
-# - Add GromacsNative class, that doesn't run based on the container but on native CVMFS mount. Test @generoso
-
 class GromacsBase(rfm.RunOnlyRegressionTest):
     def __init__(self):
         self.valid_systems = ['*']
@@ -21,46 +17,73 @@ class GromacsBase(rfm.RunOnlyRegressionTest):
             sn.assert_reference(energy, energy_reference, -0.001, 0.001)
         ])
 
+        self.perf_patterns = {
+            'perf': sn.extractsingle(r'Performance:\s+(?P<perf>\S+)',
+                                     output_file, 'perf', float)
+        }
+        self.reference = {
+            '*': {
+                'perf': (None, None, None, 'ns/day')
+            }
+        }
+
         self.maintainers = ['casparvl']
 
-class GromacsContainerBase(GromacsBase):
-    def __init__(self, nsteps):
+class GromacsSizedTests(GromacsBase):
+    def __init__(self, scale):
 
         super().__init__()
 
-        self.tags = {'container'}
+        if scale == 'single':
+            self.nsteps = '10000'
+            self.num_nodes = 1
+        elif scale == 'small':
+            self.nsteps = '40000'
+            self.num_nodes = 4
+        elif scale == 'large':
+            self.nsteps = '100000'
+            self.num_nodes = 10
+
+        self.num_tasks_per_node = 16
+        self.num_tasks = self.num_tasks_per_node * self.num_nodes
+        self.tags = {scale}
+
+
+@rfm.parameterized_test(['single'], ['small'], ['large'])
+class GromacsNative(GromacsSizedTests):
+    def __init__(self, scale):
+
+        super().__init__(scale)
+
+        self.tags.add('native')
+        self.valid_prog_environs = ['*']
+
+        self.modules = ['GROMACS']
+        self.executable = 'gmx_mpi'
+        self.executable_opts = ['mdrun', '-s ion_channel.tpr', '-maxh 0.50',
+                '-resethway', '-noconfout', '-nsteps ' + self.nsteps]
+
+@rfm.parameterized_test(['single'], ['small'], ['large'])
+class GromacsContainer(GromacsSizedTests):
+    def __init__(self, scale):
+
+        super().__init__(scale)
+
+        self.tags.add('container')
         self.valid_prog_environs = ['container']
 
         self.prerun_cmds = ['source shared_alien_cache_minimal.sh > /dev/null']
 
         self.container_platform = 'Singularity'
-        self.container_platform.image = 'docker://eessi/client-pilot:centos7-$(uname -m)-2020.10'
+        self.container_platform.image = 'docker://eessi/client-pilot:centos7-$(uname -m)'
         self.container_platform.options = [
             '--fusemount "container:cvmfs2 cvmfs-config.eessi-hpc.org /cvmfs/cvmfs-config.eessi-hpc.org"',
             '--fusemount "container:cvmfs2 pilot.eessi-hpc.org /cvmfs/pilot.eessi-hpc.org"'
         ]
 
         self.container_platform.commands = [
-            'source /cvmfs/pilot.eessi-hpc.org/2020.12/init/bash',
+            'source /cvmfs/pilot.eessi-hpc.org/latest/init/bash',
             'module load GROMACS',
             'which gmx_mpi',
-            'gmx_mpi mdrun -s ion_channel.tpr -maxh 0.50 -resethway -noconfout -nsteps ' + nsteps
+            'gmx_mpi mdrun -s ion_channel.tpr -maxh 0.50 -resethway -noconfout -nsteps ' + self.nsteps
         ]
-
-@rfm.parameterized_test(['single'], ['small'], ['large'])
-class GromacsContainer(GromacsContainerBase):
-    def __init__(self, scale):
-         
-        if scale == 'single':
-            super().__init__('10000')
-            self.num_nodes = 1
-        elif scale == 'small':
-            super().__init__('40000')
-            self.num_nodes = 4
-        elif scale == 'large':
-            super().__init__('100000')
-            self.num_nodes = 10
-
-        self.num_tasks = 24 * self.num_nodes
-        self.num_tasks_per_node = 24
-        self.tags.add(scale)
