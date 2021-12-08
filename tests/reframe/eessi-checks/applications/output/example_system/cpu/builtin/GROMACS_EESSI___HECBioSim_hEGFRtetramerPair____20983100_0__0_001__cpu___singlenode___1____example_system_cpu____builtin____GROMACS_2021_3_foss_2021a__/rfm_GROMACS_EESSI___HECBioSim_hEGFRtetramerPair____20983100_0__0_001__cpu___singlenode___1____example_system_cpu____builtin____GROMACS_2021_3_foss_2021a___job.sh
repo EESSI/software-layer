794ffb47646e6c29673996a96c49993ebcc8363e
Caspar van Leeuwen <casparl@int1.local.snellius.surf.nl>
2021-12-08 18:08:33 +0100

    Added GROMACS check written on top of CSCS GROMACS libtest

---
+++ tests/reframe/eessi-checks/applications/output/example_system/cpu/builtin/GROMACS_EESSI___HECBioSim_hEGFRtetramerPair____20983100_0__0_001__cpu___singlenode___1____example_system_cpu____builtin____GROMACS_2021_3_foss_2021a__/rfm_GROMACS_EESSI___HECBioSim_hEGFRtetramerPair____20983100_0__0_001__cpu___singlenode___1____example_system_cpu____builtin____GROMACS_2021_3_foss_2021a___job.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name="rfm_GROMACS_EESSI___HECBioSim_hEGFRtetramerPair____20983100_0__0_001__cpu___singlenode___1____example_system_cpu____builtin____GROMACS_2021_3_foss_2021a___job"
+#SBATCH --ntasks=128
+#SBATCH --ntasks-per-node=128
+#SBATCH --cpus-per-task=1
+#SBATCH --output=rfm_GROMACS_EESSI___HECBioSim_hEGFRtetramerPair____20983100_0__0_001__cpu___singlenode___1____example_system_cpu____builtin____GROMACS_2021_3_foss_2021a___job.out
+#SBATCH --error=rfm_GROMACS_EESSI___HECBioSim_hEGFRtetramerPair____20983100_0__0_001__cpu___singlenode___1____example_system_cpu____builtin____GROMACS_2021_3_foss_2021a___job.err
+#SBATCH -p thin
+module load GROMACS/2021.3-foss-2021a
+curl -LJO https://github.com/victorusu/GROMACS_Benchmark_Suite/raw/1.0.0/HECBioSim/hEGFRtetramerPair/benchmark.tpr
+srun gmx_mpi mdrun -nb cpu -s benchmark.tpr
