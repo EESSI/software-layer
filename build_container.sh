e17be5fb9f898d610b7dd55dd8668ea713e5c345
Kenneth Hoste <kenneth.hoste@ugent.be>
2022-01-15 19:18:21 +0100

    rename start_build_node_env.sh to build_container.sh and update it to also support running commands in build container

--- build_container.sh
+++ build_container.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <path for temporary directories>" >&2
+BUILD_CONTAINER="docker://ghcr.io/eessi/build-node:debian10"
+
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 <shell|run> <path for temporary directories>" >&2
     exit 1
 fi
-export EESSI_TMPDIR=$1
+SHELL_OR_RUN=$1
+EESSI_TMPDIR=$2
+shift 2
+
+if [ "$SHELL_OR_RUN" == "run" ] && [ -z "$@" ]; then
+    echo "ERROR: No command specified to run?!" >&2
+    exit 1
+fi
 
 # make sure specified temporary directory exists
 mkdir -p $EESSI_TMPDIR
@@ -39,6 +48,14 @@ export SINGULARITY_HOME="$EESSI_TMPDIR/home:/home/$USER"
 export EESSI_PILOT_READONLY="container:cvmfs2 pilot.eessi-hpc.org /cvmfs_ro/pilot.eessi-hpc.org"
 export EESSI_PILOT_WRITABLE_OVERLAY="container:fuse-overlayfs -o lowerdir=/cvmfs_ro/pilot.eessi-hpc.org -o upperdir=$EESSI_TMPDIR/overlay-upper -o workdir=$EESSI_TMPDIR/overlay-work /cvmfs/pilot.eessi-hpc.org"
 
-# start shell in Singularity container, with EESSI repository mounted with writable overlay
-echo "Starting Singularity container..."
-singularity shell --fusemount "$EESSI_PILOT_READONLY" --fusemount "$EESSI_PILOT_WRITABLE_OVERLAY" docker://ghcr.io/eessi/build-node:debian10
+if [ "$SHELL_OR_RUN" == "shell" ]; then
+    # start shell in Singularity container, with EESSI repository mounted with writable overlay
+    echo "Starting Singularity build container..."
+    singularity shell --fusemount "$EESSI_PILOT_READONLY" --fusemount "$EESSI_PILOT_WRITABLE_OVERLAY" $BUILD_CONTAINER
+elif [ "$SHELL_OR_RUN" == "run" ]; then
+    echo "Running '$@' in Singularity build container..."
+    singularity exec --fusemount "$EESSI_PILOT_READONLY" --fusemount "$EESSI_PILOT_WRITABLE_OVERLAY" $BUILD_CONTAINER "$@"
+else
+    echo "ERROR: Unknown action specified: $SHELL_OR_RUN (should be either 'shell' or 'run')" >&2
+    exit 1
+fi
