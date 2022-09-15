e796e4f26549be77de1c4c2874eefe8cf666c12a
hugo meiland <hugo.meiland@microsoft.com>
2022-09-15 11:04:39 +0200

    code cleanup

--- init/eessi_archdetect.sh
+++ init/eessi_archdetect.sh
@@ -27,9 +27,9 @@ cpupath () {
 
   if [ ${MACHINE_TYPE} == "ppc64le" ]; then
     CPU_FLAGS=$(grep -m 1 -i ^cpu ${PROC_CPUINFO})
-    [[ $CPU_FLAGS =~ " POWER9 " ]] && HAS_POWER9=true
+    [[ $CPU_FLAGS =~ " POWER9 " ]] && EESSI_HAS_POWER9=true
 
-    [[ ${HAS_POWER9} ]] && CPU_PATH=${MACHINE_TYPE}/power9le
+    [[ ${EESSI_HAS_POWER9} ]] && CPU_PATH=${MACHINE_TYPE}/power9le
     echo ${CPU_PATH}
     exit
   fi
@@ -37,31 +37,31 @@ cpupath () {
   if [ ${MACHINE_TYPE} == "x86_64" ]; then
     # check for vendor info, if available, for x86_64
     CPUINFO_VENDOR_FLAG=$(grep -m 1 -i ^vendor ${PROC_CPUINFO})
-    [[ $CPUINFO_VENDOR_FLAG =~ "GenuineIntel" ]] && CPU_VENDOR=intel
-    [[ $CPUINFO_VENDOR_FLAG =~ "AuthenticAMD" ]] && CPU_VENDOR=amd
+    [[ $CPUINFO_VENDOR_FLAG =~ "GenuineIntel" ]] && EESSI_CPU_VENDOR=intel
+    [[ $CPUINFO_VENDOR_FLAG =~ "AuthenticAMD" ]] && EESSI_CPU_VENDOR=amd
 
     CPU_FLAGS=$(grep -m 1 -i ^flags ${PROC_CPUINFO} | sed 's/$/ /g')
-    [[ $CPU_FLAGS =~ " avx2 " ]] && HAS_AVX2=true
-    [[ $CPU_FLAGS =~ " fma " ]] && HAS_FMA=true
-    [[ $CPU_FLAGS =~ " avx512f " ]] && HAS_AVX512F=true
-    [[ $CPU_FLAGS =~ " avx512vl " ]] && HAS_AVX512VL=true
-    [[ $CPU_FLAGS =~ " avx512ifma " ]] && HAS_AVX512IFMA=true
-    [[ $CPU_FLAGS =~ " avx512_vbmi2 " ]] && HAS_AVX512_VBMI2=true
-    [[ $CPU_FLAGS =~ " avx512_vnni " ]] && HAS_AVX512_VNNI=true
-    [[ $CPU_FLAGS =~ " avx512fp16 " ]] && HAS_AVX512FP16=true
-    [[ $CPU_FLAGS =~ " vaes " ]] && HAS_VAES=true
+    [[ $CPU_FLAGS =~ " avx2 " ]] && EESSI_HAS_AVX2=true
+    [[ $CPU_FLAGS =~ " fma " ]] && EESSI_HAS_FMA=true
+    [[ $CPU_FLAGS =~ " avx512f " ]] && EESSI_HAS_AVX512F=true
+    [[ $CPU_FLAGS =~ " avx512vl " ]] && EESSI_HAS_AVX512VL=true
+    [[ $CPU_FLAGS =~ " avx512ifma " ]] && EESSI_HAS_AVX512IFMA=true
+    [[ $CPU_FLAGS =~ " avx512_vbmi2 " ]] && EESSI_HAS_AVX512_VBMI2=true
+    [[ $CPU_FLAGS =~ " avx512_vnni " ]] && EESSI_HAS_AVX512_VNNI=true
+    [[ $CPU_FLAGS =~ " avx512fp16 " ]] && EESSI_HAS_AVX512FP16=true
+    [[ $CPU_FLAGS =~ " vaes " ]] && EESSI_HAS_VAES=true
 
-    if [ ${CPU_VENDOR} == "intel" ]; then
-      [[ ${HAS_AVX2} ]] && [[ ${HAS_FMA} ]] && CPU_TYPE=haswell 
-      [[ ${HAS_AVX512F} ]] && CPU_TYPE=skylake_avx512
+    if [ ${EESSI_CPU_VENDOR} == "intel" ]; then
+      [[ ${EESSI_HAS_AVX2} ]] && [[ ${EESSI_HAS_FMA} ]] && EESSI_CPU_TYPE=haswell 
+      [[ ${EESSI_HAS_AVX512F} ]] && EESSI_CPU_TYPE=skylake_avx512
       # [[ ${HAS_AVX512IFMA} ]] && [[ ${HAS_AVX512_VBMI2} ]] && CPU_TYPE=icelake_avx512
       # [[ ${HAS_AVX512_VNNI} ]] && [[ ${HAS_AVX512VL} ]] && [[ ${HAS_AVX512FP16} ]] && CPU_TYPE=sapphire_rapids_avx512
-    elif [ ${CPU_VENDOR} == "amd" ]; then
-      [[ ${HAS_AVX2} ]] && [[ ${HAS_FMA} ]] && CPU_TYPE=zen2
-      [[ ${HAS_AVX2} ]] && [[ ${HAS_FMA} ]] && [[ ${HAS_VAES} ]] && CPU_TYPE=zen3
+    elif [ ${EESSI_CPU_VENDOR} == "amd" ]; then
+      [[ ${EESSI_HAS_AVX2} ]] && [[ ${EESSI_HAS_FMA} ]] && CPU_TYPE=zen2
+      [[ ${EESSI_HAS_AVX2} ]] && [[ ${EESSI_HAS_FMA} ]] && [[ ${EESSI_HAS_VAES} ]] && CPU_TYPE=zen3
     fi
 
-    [[ ${CPU_VENDOR} ]] && [[ $CPU_TYPE ]] && CPU_PATH="${MACHINE_TYPE}/${CPU_VENDOR}/${CPU_TYPE}"
+    [[ ${EESSI_CPU_VENDOR} ]] && [[ $EESSI_CPU_TYPE ]] && CPU_PATH="${MACHINE_TYPE}/${EESSI_CPU_VENDOR}/${EESSI_CPU_TYPE}"
 
     echo ${CPU_PATH}
     exit
