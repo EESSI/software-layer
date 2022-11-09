deb34a761a545609693966dc37a323ab75553fa6
Kenneth Hoste <kenneth.hoste@ugent.be>
2022-11-09 23:24:40 +0100

    add OSU-Micro-Benchmarks/5.7.1-gompi-2021a to EESSI pilot 2021.12

--- EESSI-pilot-install-software.sh
+++ EESSI-pilot-install-software.sh
@@ -362,6 +362,12 @@ fail_msg="Installation of Nextflow failed, that's unexpected..."
 $EB -r --from-pr 16531 Nextflow-22.10.1.eb
 check_exit_code $? "${ok_msg}" "${fail_msg}"
 
+echo ">> Installing OSU-Micro-Benchmarks/5.7.1-gompi-2021a..."
+ok_msg="OSU-Micro-Benchmarks installed, yihaa!"
+fail_msg="Installation of OSU-Micro-Benchmarks failed, that's unexpected..."
+$EB OSU-Micro-Benchmarks-5.7.1-gompi-2021a.eb -r
+check_exit_code $? "${ok_msg}" "${fail_msg}"
+
 ### add packages here
 
 echo ">> Creating/updating Lmod cache..."
