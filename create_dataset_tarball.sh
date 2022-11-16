0db6db1079e3687640c923646a941c1acf966fc8
hugo meiland <hugo.meiland@microsoft.com>
2022-11-16 06:23:21 +0100

    removed datasets topdir; using data.eessi-hpc.org/test to verify mount

--- create_dataset_tarball.sh
+++ create_dataset_tarball.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-EESSI_ROOT=/cvmfs/pilot.eessi-hpc.org/
+EESSI_ROOT=/cvmfs/data.eessi-hpc.org/
 TMP_DIR=/tmp/$USER
 
 # checking if all requirements are here
@@ -14,7 +14,7 @@ if [ ! -f eessi-datasets.yml ]; then
 	echo "missing eessi-datasets.yml, can not continue without..." && exit -1
 fi
 
-if [ ! -d ${EESSI_ROOT}/datasets ]; then
+if [ ! -f ${EESSI_ROOT}/test ]; then
 # if [ ! -d ${EESSI_ROOT}/ ]; then #used for testing while datasets was not in eessi repo
 	echo "missing EESSI cvmfs mount, can not continue without..." && exit -1
 fi
@@ -28,46 +28,45 @@ if [ -d ${TMP_DIR}/${EESSI_ROOT} ]; then
 fi
 
 #looping over yaml file:
-#datasets:
-#  WRF:
-#   versions:
-#    "3.9.1.1":
-#      - conus12km_v3911
+#WRF:
+# versions:
+#  "3.9.1.1":
+#    - conus12km_v3911
 
 
 # read in the application list, for which to ingest datasets
-SOFTWARE=`yq e '.datasets | keys' eessi-datasets.yml | awk {'print $2'}`
+SOFTWARE=`yq e '. | keys' eessi-datasets.yml | awk {'print $2'}`
 for APPLICATION in ${SOFTWARE}; do
     echo "checking path for ${APPLICATION}"
-    if [ ! -d ${EESSI_ROOT}/datasets/${APPLICATION} ]; then
-        mkdir -p ${TMP_DIR}/${EESSI_ROOT}/datasets/${APPLICATION} || echo "failure to create tmp dir" 
+    if [ ! -d ${EESSI_ROOT}/${APPLICATION} ]; then
+        mkdir -p ${TMP_DIR}/${EESSI_ROOT}/${APPLICATION} || echo "failure to create tmp dir" 
     fi
   
-    SOFTWARE_VERSIONS=`yq e ".datasets.${APPLICATION}.versions | keys" eessi-datasets.yml | awk {'print $2'}`
+    SOFTWARE_VERSIONS=`yq e ".${APPLICATION}.versions | keys" eessi-datasets.yml | awk {'print $2'}`
     for VERSION in ${SOFTWARE_VERSIONS}; do
         echo "checking path for version ${VERSION:1:-1}"
-        if [ ! -d ${EESSI_ROOT}/datasets/${APPLICATION}/${VERSION:1:-1} ]; then
-	    mkdir -p ${TMP_DIR}/${EESSI_ROOT}/datasets/${APPLICATION}/${VERSION:1:-1} || echo "failure to create tmp dir" 
+        if [ ! -d ${EESSI_ROOT}/${APPLICATION}/${VERSION:1:-1} ]; then
+	    mkdir -p ${TMP_DIR}/${EESSI_ROOT}/${APPLICATION}/${VERSION:1:-1} || echo "failure to create tmp dir" 
         fi
 
-        DATASETS=`yq e ".datasets.${APPLICATION}.versions[${VERSION}]" eessi-datasets.yml | awk {'print $2'}`
+        DATASETS=`yq e ".${APPLICATION}.versions[${VERSION}]" eessi-datasets.yml | awk {'print $2'}`
         for DATASET in ${DATASETS}; do
             echo "checking path for dataset ${DATASET}"
-            if [ -d ${EESSI_ROOT}/datasets/${APPLICATION}/${VERSION:1:-1}/${DATASET} ]; then
+            if [ -d ${EESSI_ROOT}/${APPLICATION}/${VERSION:1:-1}/${DATASET} ]; then
                 echo "path for dataset ${DATASET} seems to exists; skipping...."
 	        continue	
 	    else
-	        mkdir -p ${TMP_DIR}/${EESSI_ROOT}/datasets/${APPLICATION}/${VERSION:1:-1}/${DATASET} || echo "failure to create tmp dir" 
+	        mkdir -p ${TMP_DIR}/${EESSI_ROOT}/${APPLICATION}/${VERSION:1:-1}/${DATASET} || echo "failure to create tmp dir" 
             fi
             if [ ! -f datasets/${APPLICATION}/${VERSION:1:-1}/${DATASET}/download.sh ]; then
                 echo "download script for ${DATASET} does not seem to exist....please fix repo first"
 		exit -1
             fi
 	    # running download script with target dir as variable
-	    datasets/${APPLICATION}/${VERSION:1:-1}/${DATASET}/download.sh ${TMP_DIR}/${EESSI_ROOT}/datasets/${APPLICATION}/${VERSION:1:-1}/${DATASET} ${PWD}/datasets/${APPLICATION}/${VERSION:1:-1}/${DATASET}/
+	    datasets/${APPLICATION}/${VERSION:1:-1}/${DATASET}/download.sh ${TMP_DIR}/${EESSI_ROOT}/${APPLICATION}/${VERSION:1:-1}/${DATASET} ${PWD}/datasets/${APPLICATION}/${VERSION:1:-1}/${DATASET}/
 
 	    # generating filelist
-            pushd ${TMP_DIR}/${EESSI_ROOT}/datasets
+            pushd ${TMP_DIR}/${EESSI_ROOT}
             find ${APPLICATION}/${VERSION:1:-1}/${DATASET} >> ${TMP_DIR}/dataset_filelist.txt
 	    popd
 
@@ -75,7 +74,7 @@ for APPLICATION in ${SOFTWARE}; do
     done #VERSION
 done #APPLICATION
 
-tar cfvz eessi-dataset-$(date +%s).tgz -C ${TMP_DIR}/${EESSI_ROOT}/datasets --files-from=${TMP_DIR}/dataset_filelist.txt
+tar cfvz eessi-dataset-$(date +%s).tgz -C ${TMP_DIR}/${EESSI_ROOT} --files-from=${TMP_DIR}/dataset_filelist.txt
 
 # cleaning up afterwards
 rm ${TMP_DIR}/dataset_filelist.txt
