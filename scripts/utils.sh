95dc24509434d38e353f21a54413d771ebfdc401
Alan O'Cais <alan.ocais@cecam.org>
2023-03-03 11:56:02 +0100

    Use realpath to be consistent with other scripts

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -45,13 +45,13 @@ function create_directory_structure() {
   return_code=$?
   # If it fails be explicit about the error
   if [ ${return_code} -ne 0 ]; then
-    real_dir=$(readlink -m "$dir_structure")
+    real_dir=$(realpath -s "$dir_structure")
     echo_red "Creating ${dir_structure} (real path ${real_dir}) failed with:\n ${error_message}" >&2
   else
     # If we're creating it, our use case is that we want to be able to write there
     # (this is a check in case the directory already existed)
     if [ ! -w "${dir_structure}" ]; then
-      real_dir=$(readlink -m "$dir_structure")
+      real_dir=$(realpath -s "$dir_structure")
       echo_red "You do not have (required) write permissions to ${dir_structure} (real path ${real_dir})!"
       return_code=$ANY_ERROR_EXITCODE
     fi
