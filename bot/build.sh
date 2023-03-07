d08bd211ed6c23c5b917b01bd01ef75a5cb7b1b9
trz42 <trz42@users.noreply.github.com>
2023-03-07 14:43:52 +0100

    addressed comments and changes requested by first review

--- bot/build.sh
+++ bot/build.sh
@@ -16,41 +16,44 @@
 #    pull request (OR by some other means)
 #  - the working directory contains a directory 'cfg' where the main config
 #    file 'job.cfg' has been deposited
-#  - the directory may contain any additional files references in job.cfg
+#  - the directory may contain any additional files referenced in job.cfg
 
-# defaults
-export JOB_CFG_FILE="${JOB_CFG_FILE_OVERRIDE:=./cfg/job.cfg}"
-
-echo "bot/build.sh: Showing job.cfg from software-layer side"
-cat cfg/job.cfg
+# stop as soon as something fails
+set -e
 
 # source utils.sh and cfg_files.sh
 source scripts/utils.sh
 source scripts/cfg_files.sh
 
-# check if './cfg/job.cfg' exists
+# defaults
+export JOB_CFG_FILE="${JOB_CFG_FILE_OVERRIDE:=./cfg/job.cfg}"
+
+# check if ${JOB_CFG_FILE} exists
 if [[ ! -r "${JOB_CFG_FILE}" ]]; then
     fatal_error "job config file (JOB_CFG_FILE=${JOB_CFG_FILE}) does not exist or not readable"
 fi
-echo "obtaining configuration settings from '${JOB_CFG_FILE}'"
+echo "bot/build.sh: showing ${JOB_CFG_FILE} from software-layer side"
+cat ${JOB_CFG_FILE}
+
+echo "bot/build.sh: obtaining configuration settings from '${JOB_CFG_FILE}'"
 cfg_load ${JOB_CFG_FILE}
 
-# if http_proxy is in cfg/job.cfg use it, if not use env var $http_proxy
+# if http_proxy is defined in ${JOB_CFG_FILE} use it, if not use env var $http_proxy
 HTTP_PROXY=$(cfg_get_value "site_config" "http_proxy")
 HTTP_PROXY=${HTTP_PROXY:-${http_proxy}}
-echo "HTTP_PROXY='${HTTP_PROXY}'"
+echo "bot/build.sh: HTTP_PROXY='${HTTP_PROXY}'"
 
-# if https_proxy is in cfg/job.cfg use it, if not use env var $https_proxy
+# if https_proxy is defined in ${JOB_CFG_FILE} use it, if not use env var $https_proxy
 HTTPS_PROXY=$(cfg_get_value "site_config" "https_proxy")
 HTTPS_PROXY=${HTTPS_PROXY:-${https_proxy}}
-echo "HTTPS_PROXY='${HTTPS_PROXY}'"
+echo "bot/build.sh: HTTPS_PROXY='${HTTPS_PROXY}'"
 
 LOCAL_TMP=$(cfg_get_value "site_config" "local_tmp")
-echo "LOCAL_TMP='${LOCAL_TMP}'"
+echo "bot/build.sh: LOCAL_TMP='${LOCAL_TMP}'"
 # TODO should local_tmp be mandatory? --> then we check here and exit if it is not provided
 
 SINGULARITY_CACHEDIR=$(cfg_get_value "site_config" "container_cachedir")
-echo "SINGULARITY_CACHEDIR='${SINGULARITY_CACHEDIR}'"
+echo "bot/build.sh: SINGULARITY_CACHEDIR='${SINGULARITY_CACHEDIR}'"
 if [[ ! -z ${SINGULARITY_CACHEDIR} ]]; then
     export SINGULARITY_CACHEDIR
 fi
@@ -61,14 +64,21 @@ echo -n "setting \$STORAGE by replacing any var in '${LOCAL_TMP}' -> "
 STORAGE=$(envsubst <<< ${LOCAL_TMP})
 echo "'${STORAGE}'"
 
+# make sure ${STORAGE} exists
+mkdir -p ${STORAGE}
+
+# make sure the base tmp storage is unique
+JOB_STORAGE=$(mktemp --directory --tmpdir=${STORAGE} bot_job_tmp_XXX)
+echo "bot/build.sh: created unique base tmp storage directory at ${JOB_STORAGE}"
+
 # obtain list of modules to be loaded
 LOAD_MODULES=$(cfg_get_value "site_config" "load_modules")
-echo "LOAD_MODULES='${LOAD_MODULES}'"
+echo "bot/build.sh: LOAD_MODULES='${LOAD_MODULES}'"
 
 # singularity/apptainer settings: CONTAINER, HOME, TMPDIR, BIND
 CONTAINER=$(cfg_get_value "repository" "container")
-export SINGULARITY_HOME="$(pwd):/eessi_bot_job"
-export SINGULARITY_TMPDIR="$(pwd)/singularity_tmpdir"
+export SINGULARITY_HOME="${pwd}:/eessi_bot_job"
+export SINGULARITY_TMPDIR="${pwd}/singularity_tmpdir"
 mkdir -p ${SINGULARITY_TMPDIR}
 
 # load modules if LOAD_MODULES is not empty
@@ -82,103 +92,91 @@ else
     echo "bot/build.sh: no modules to be loaded"
 fi
 
-# determine repository to be used from entry .repository in cfg/job.cfg
+# determine repository to be used from entry .repository in ${JOB_CFG_FILE}
 REPOSITORY=$(cfg_get_value "repository" "repo_id")
 EESSI_REPOS_CFG_DIR_OVERRIDE=$(cfg_get_value "repository" "repos_cfg_dir")
 export EESSI_REPOS_CFG_DIR_OVERRIDE=${EESSI_REPOS_CFG_DIR_OVERRIDE:-${PWD}/cfg}
+echo "bot/build.sh: EESSI_REPOS_CFG_DIR_OVERRIDE='${EESSI_REPOS_CFG_DIR_OVERRIDE}'"
 
-# determine pilot version to be used from .repository.repo_version in cfg/job.cfg
+# determine pilot version to be used from .repository.repo_version in ${JOB_CFG_FILE}
 # here, just set & export EESSI_PILOT_VERSION_OVERRIDE
 # next script (eessi_container.sh) makes use of it via sourcing init scripts
 # (e.g., init/eessi_defaults or init/minimal_eessi_env)
 export EESSI_PILOT_VERSION_OVERRIDE=$(cfg_get_value "repository" "repo_version")
+echo "bot/build.sh: EESSI_PILOT_VERSION_OVERRIDE='${EESSI_PILOT_VERSION_OVERRIDE}'"
 
-# determine CVMFS repo to be used from .repository.repo_name in cfg/job.cfg
+# determine CVMFS repo to be used from .repository.repo_name in ${JOB_CFG_FILE}
 # here, just set EESSI_CVMFS_REPO_OVERRIDE, a bit further down
 # "source init/eessi_defaults" via sourcing init/minimal_eessi_env
 export EESSI_CVMFS_REPO_OVERRIDE=$(cfg_get_value "repository" "repo_name")
+echo "bot/build.sh: EESSI_CVMFS_REPO_OVERRIDE='${EESSI_CVMFS_REPO_OVERRIDE}'"
 
-
-# determine architecture to be used from entry .architecture in cfg/job.cfg
-# default: leave empty to let downstream script(s) determine subdir to be used
-if [[ ! -z "${CPU_TARGET}" ]]; then
-    EESSI_SOFTWARE_SUBDIR_OVERRIDE=${CPU_TARGET}
-else
-    EESSI_SOFTWARE_SUBDIR_OVERRIDE=$(cfg_get_value "architecture" "software_subdir")
-fi
+# determine architecture to be used from entry .architecture in ${JOB_CFG_FILE}
+# fallbacks:
+#  - ${CPU_TARGET} handed over from bot
+#  - left empty to let downstream script(s) determine subdir to be used
+EESSI_SOFTWARE_SUBDIR_OVERRIDE=$(cfg_get_value "architecture" "software_subdir")
+EESSI_SOFTWARE_SUBDIR_OVERRIDE=${EESSI_SOFTWARE_SUBDIR_OVERRIDE:-${CPU_TARGET}}
 export EESSI_SOFTWARE_SUBDIR_OVERRIDE
+echo "bot/build.sh: EESSI_SOFTWARE_SUBDIR_OVERRIDE='${EESSI_SOFTWARE_SUBDIR_OVERRIDE}'"
 
-# get EESSI_OS_TYPE from .architecture.os_type in cfg/job.cfg (default: linux)
+# get EESSI_OS_TYPE from .architecture.os_type in ${JOB_CFG_FILE} (default: linux)
 EESSI_OS_TYPE=$(cfg_get_value "architecture" "os_type")
 export EESSI_OS_TYPE=${EESSI_OS_TYPE:-linux}
+echo "bot/build.sh: EESSI_OS_TYPE='${EESSI_OS_TYPE}'"
 
-# TODO
-#   - CODED add handling of EESSI_SOFTWARE_SUBDIR_OVERRIDE to eessi_container.sh
-#     TODO ensure that the bot makes use of that. (currently sets env var
-#     CPU_TARGET & adds --export=ALL,CPU_TARGET=val to sbatch command ... also
-#     add it to cfg/job.cfg - .architecture.software_subdir)
-#   - CODED add handling of http(s)_proxy to eessi_container.sh, in there needs the
-#     CVMFS_HTTP_PROXY added to /etc/cvmfs/default.local (this needs a robust
-#     way to determine the IP address of a proxy)
-#   - bot needs to make repos.cfg and cfg_bundle available to job (likely, by copying
-#     files into './cfg/.' and defining '.repository.repos_cfg_dir' in './cfg/job.cfg')
+# prepare arguments to eessi_container.sh common to build and tarball steps
+declare -a COMMON_ARGS=()
+COMMON_ARGS+=("--verbose")
+COMMON_ARGS+=("--access" "rw")
+COMMON_ARGS+=("--mode" "run")
+[[ ! -z ${CONTAINER} ]] && COMMON_ARGS+=("--container" "${CONTAINER}")
+[[ ! -z ${HTTP_PROXY} ]] && COMMON_ARGS+=("--http-proxy" "${HTTP_PROXY}")
+[[ ! -z ${HTTPS_PROXY} ]] && COMMON_ARGS+=("--https-proxy" "${HTTPS_PROXY}")
+[[ ! -z ${REPOSITORY} ]] && COMMON_ARGS+=("--repository" "${REPOSITORY}")
 
-# prepare options and directories for calling eessi_container.sh
+# make sure to use the same parent dir for storing tarballs of tmp
+PREVIOUS_TMP_DIR=${PWD}/previous_tmp
+
+# prepare directory to store tarball of tmp for build step
+TARBALL_TMP_BUILD_STEP_DIR=${PREVIOUS_TMP_DIR}/build_step
+mkdir -p ${TARBALL_TMP_BUILD_STEP_DIR}
+
+# prepare arguments to eessi_container.sh specific to build step
 declare -a BUILD_STEP_ARGS=()
-BUILD_STEP_ARGS+=("--verbose")
-BUILD_STEP_ARGS+=("--access" "rw")
-BUILD_STEP_ARGS+=("--mode" "run")
-BUILD_STEP_ARGS+=("--save" "${PWD}/previous_tmp/build_step")
+BUILD_STEP_ARGS+=("--save" "${TARBALL_TMP_BUILD_STEP_DIR}")
 BUILD_STEP_ARGS+=("--storage" "${STORAGE}")
-declare -a TARBALL_STEP_ARGS=()
-TARBALL_STEP_ARGS+=("--verbose")
-TARBALL_STEP_ARGS+=("--access" "rw")
-TARBALL_STEP_ARGS+=("--mode" "run")
-TARBALL_STEP_ARGS+=("--save" "${PWD}/previous_tmp/tarball_step")
-CONTAINER_OPT=
-if [[ ! -z ${CONTAINER} ]]; then
-    CONTAINER_OPT="--container ${CONTAINER}"
-    BUILD_STEP_ARGS+=("--container" "${CONTAINER}")
-    TARBALL_STEP_ARGS+=("--container" "${CONTAINER}")
-fi
-HTTP_PROXY_OPT=
-if [[ ! -z ${HTTP_PROXY} ]]; then
-    HTTP_PROXY_OPT="--http-proxy ${HTTP_PROXY}"
-    BUILD_STEP_ARGS+=("--http-proxy" "${HTTP_PROXY}")
-    TARBALL_STEP_ARGS+=("--http-proxy" "${HTTP_PROXY}")
-fi
-HTTPS_PROXY_OPT=
-if [[ ! -z ${HTTPS_PROXY} ]]; then
-    HTTPS_PROXY_OPT="--https-proxy ${HTTPS_PROXY}"
-    BUILD_STEP_ARGS+=("--https-proxy" "${HTTPS_PROXY}")
-    TARBALL_STEP_ARGS+=("--https-proxy" "${HTTPS_PROXY}")
-fi
-REPOSITORY_OPT=
-if [[ ! -z ${REPOSITORY} ]]; then
-    REPOSITORY_OPT="--repository ${REPOSITORY}"
-    BUILD_STEP_ARGS+=("--repository" "${REPOSITORY}")
-    TARBALL_STEP_ARGS+=("--repository" "${REPOSITORY}")
-fi
+
+# prepare arguments to install_software_layer.sh (specific to build step)
 GENERIC_OPT=
 if [[ ${EESSI_SOFTWARE_SUBDIR_OVERRIDE} =~ .*/generic$ ]]; then
     GENERIC_OPT="--generic"
 fi
 
-mkdir -p previous_tmp/{build_step,tarball_step}
+# create tmp file for output of build step
 build_outerr=$(mktemp build.outerr.XXXX)
+
 echo "Executing command to build software:"
-echo "./eessi_container.sh ${BUILD_STEP_ARGS[@]}"
+echo "./eessi_container.sh ${COMMON_ARGS[@]} ${BUILD_STEP_ARGS[@]}"
 echo "                     -- ./install_software_layer.sh ${GENERIC_OPT} \"$@\" 2>&1 | tee -a ${build_outerr}"
-# set EESSI_REPOS_CFG_DIR_OVERRIDE to ./cfg
-export EESSI_REPOS_CFG_DIR_OVERRIDE=${PWD}/cfg
-./eessi_container.sh "${BUILD_STEP_ARGS[@]}" \
+./eessi_container.sh "${COMMON_ARGS[@]}" "${BUILD_STEP_ARGS[@]}" \
                      -- ./install_software_layer.sh ${GENERIC_OPT} "$@" 2>&1 | tee -a ${build_outerr}
+
+# prepare directory to store tarball of tmp for tarball step
+TARBALL_TMP_TARBALL_STEP_DIR=${PREVIOUS_TMP_DIR}/tarball_step
+mkdir -p ${TARBALL_TMP_TARBALL_STEP_DIR}
+
+# create tmp file for output of tarball step
+tar_outerr=$(mktemp tar.outerr.XXXX)
+
+# prepare arguments to eessi_container.sh specific to tarball step
+declare -a TARBALL_STEP_ARGS=()
+TARBALL_STEP_ARGS+=("--save" "${TARBALL_TMP_TARBALL_STEP_DIR}")
 
 # determine temporary directory to resume from
 BUILD_TMPDIR=$(grep ' as tmp directory ' ${build_outerr} | cut -d ' ' -f 2)
 TARBALL_STEP_ARGS+=("--resume" "${BUILD_TMPDIR}")
 
-tar_outerr=$(mktemp tar.outerr.XXXX)
 timestamp=$(date +%s)
 # to set EESSI_PILOT_VERSION we need to source init/eessi_defaults now
 source init/eessi_defaults
@@ -190,15 +188,9 @@ export TGZ=$(printf "eessi-%s-software-%s-%s-%d.tar.gz" ${EESSI_PILOT_VERSION} $
 # /tmp as default?
 TMP_IN_CONTAINER=/tmp
 echo "Executing command to create tarball:"
-echo "./eessi_container.sh ${TARBALL_STEP_ARGS[@]}"
+echo "./eessi_container.sh ${COMMON_ARGS[@]} ${TARBALL_STEP_ARGS[@]}"
 echo "                     -- ./create_tarball.sh ${TMP_IN_CONTAINER} ${EESSI_PILOT_VERSION} ${EESSI_SOFTWARE_SUBDIR_OVERRIDE} /eessi_bot_job/${TGZ} 2>&1 | tee -a ${tar_outerr}"
-./eessi_container.sh "${TARBALL_STEP_ARGS[@]}" \
+./eessi_container.sh "${COMMON_ARGS[@]}" "${TARBALL_STEP_ARGS[@]}" \
                      -- ./create_tarball.sh ${TMP_IN_CONTAINER} ${EESSI_PILOT_VERSION} ${EESSI_SOFTWARE_SUBDIR_OVERRIDE} /eessi_bot_job/${TGZ} 2>&1 | tee -a ${tar_outerr}
-
-# if two tarballs have been generated, only keep the one from tarball step
-NUM_TARBALLS=$(find ${PWD}/previous_tmp -type f -name "*tgz" | wc -l)
-if [[ ${NUM_TARBALLS} -eq 2 ]]; then
-    rm -f previous_tmp/build_step/*.tgz
-fi
 
 exit 0
