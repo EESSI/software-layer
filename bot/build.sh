64d1866638288e315de91a41ad37a40fdaf53736
trz42 <trz42@users.noreply.github.com>
2023-03-11 01:33:50 +0100

    fix bug, env var PWD is uppercase

--- bot/build.sh
+++ bot/build.sh
@@ -77,8 +77,8 @@ echo "bot/build.sh: LOAD_MODULES='${LOAD_MODULES}'"
 
 # singularity/apptainer settings: CONTAINER, HOME, TMPDIR, BIND
 CONTAINER=$(cfg_get_value "repository" "container")
-export SINGULARITY_HOME="${pwd}:/eessi_bot_job"
-export SINGULARITY_TMPDIR="${pwd}/singularity_tmpdir"
+export SINGULARITY_HOME="${PWD}:/eessi_bot_job"
+export SINGULARITY_TMPDIR="${PWD}/singularity_tmpdir"
 mkdir -p ${SINGULARITY_TMPDIR}
 
 # load modules if LOAD_MODULES is not empty
