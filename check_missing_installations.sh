3c43a92524f4f8f6d5db167c8a58cedc273d51a0
Thomas Roeblitz <thomas.roblitz@uib.no>
2023-06-18 20:09:00 +0200

    check for missing installations requires a few updates for check-result.sh

--- check_missing_installations.sh
+++ check_missing_installations.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 #
-# Script to check for missing installations in EESSI pilot software stack (version 2021.12)
+# Script to check for missing installations in EESSI pilot software stack (version 2023.06)
 #
 # author: Kenneth Hoste (@boegel)
+# author: Thomas Roeblitz (@trz42)
 #
 # license: GPLv2
 #
 
 TOPDIR=$(dirname $(realpath $0))
 
-if [ -z ${EESSI_PILOT_VERSION} ]; then
-    echo "ERROR: \${EESSI_PILOT_VERSION} must be set to run $0!" >&2
+if [ $# -ne 1 ]; then
+    echo "ERROR: Usage: $0 <path to easystack file>" >&2
     exit 1
 fi
+easystack=$1
 
 LOCAL_TMPDIR=$(mktemp -d)
 
@@ -21,12 +23,13 @@ source $TOPDIR/scripts/utils.sh
 source $TOPDIR/configure_easybuild
 
 echo ">> Checking for missing installations in ${EASYBUILD_INSTALLPATH}..."
-ok_msg="No missing installations, party time!"
-fail_msg="On no, some installations are still missing, how did that happen?!"
 eb_missing_out=$LOCAL_TMPDIR/eb_missing.out
-# we need to use --from-pr to pull in some easyconfigs that are not available in EasyBuild version being used
-# PR #16531: Nextflow-22.10.1.eb
-${EB:-eb} --from-pr 16531 --easystack eessi-${EESSI_PILOT_VERSION}.yml --experimental --missing | tee ${eb_missing_out}
+${EB:-eb} --easystack ${easystack} --missing 2>&1 | tee ${eb_missing_out}
+exit_code=${PIPESTATUS[0]}
+
+ok_msg="Command 'eb --missing ...' succeeded, analysing output..."
+fail_msg="Command 'eb --missing ...' failed, check log '${eb_missing_out}'"
+check_exit_code ${exit_code} "${ok_msg}" "${fail_msg}"
 
 # the above assesses the installed software for each easyconfig provided in
 # the easystack file and then print messages such as
@@ -37,8 +40,11 @@ ${EB:-eb} --from-pr 16531 --easystack eessi-${EESSI_PILOT_VERSION}.yml --experim
 # output does not contain any line with ` required modules missing:`
 
 grep " required modules missing:" ${eb_missing_out} > /dev/null
+exit_code=$?
 
 # if grep returns 1 (` required modules missing:` was NOT found), we set
 # MODULES_MISSING to 0, otherwise (it was found or another error) we set it to 1
-[[ $? -eq 1 ]] && MODULES_MISSING=0 || MODULES_MISSING=1
+[[ ${exit_code} -eq 1 ]] && MODULES_MISSING=0 || MODULES_MISSING=1
+ok_msg="No missing installations, party time!"
+fail_msg="On no, some installations are still missing, how did that happen?!"
 check_exit_code ${MODULES_MISSING} "${ok_msg}" "${fail_msg}"
