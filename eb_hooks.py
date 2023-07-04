2a32626f5b3a38e3b8a0beebbc65e13761830154
TopRichard <richard.topouchian@uib.no>
2023-07-04 12:40:47 +0000

    added a hook to set check_qtwebengine to False

--- eb_hooks.py
+++ eb_hooks.py
@@ -59,9 +59,16 @@ def parse_hook(ec, *args, **kwargs):
     # determine path to Prefix installation in compat layer via $EPREFIX
     eprefix = get_eessi_envvar('EPREFIX')
 
+#Disable Qt5/5.15.2 Sanity-check
+    Qt5_Sanitycheck_disable(ec, eprefix)
+
     if ec.name in PARSE_HOOKS:
         PARSE_HOOKS[ec.name](ec, eprefix)
 
+def Qt5_Sanitycheck_disable(ec, eprefix):
+    if ec.name == 'Qt5'and ec.version == '5.15.2':
+         ec['check_qtwebengine'] = False  
+         print_msg(NOTE:the value of check_qtwebengine has been set to %s , ec['check_qtwebengine'])
 
 def pre_prepare_hook(self, *args, **kwargs):
     """Main pre-prepare hook: trigger custom functions."""
