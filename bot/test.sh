fd765bbd41691e49d8efcf044fb07e3632049d85
Kenneth Hoste <kenneth.hoste@ugent.be>
2023-11-26 21:04:47 +0100

    add dummy bot/test.sh and bot/check-test.sh scripts

---
+++ bot/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Dummy script, no tests yet
+#
+# This script is part of the EESSI software layer, see
+# https://github.com/EESSI/software-layer.git
+#
+# author: Kenneth Hoste (HPC-UGent)
+#
+# license: GPLv2
+#
+
+exit 0
