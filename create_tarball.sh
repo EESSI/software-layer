42e3404a0015d7b4049c5cd3e5a01ee764261d44
Kenneth Hoste <kenneth.hoste@ugent.be>
2023-12-21 14:13:23 +0100

    always append to list of files to include in tarball, to avoid overwriting it

--- create_tarball.sh
+++ create_tarball.sh
@@ -40,10 +40,10 @@ echo ">> Collecting list of files/directories to include in tarball via ${PWD}..
 files_list=${tmpdir}/files.list.txt
 module_files_list=${tmpdir}/module_files.list.txt
 
-# include scripts that were copied by install_scripts.sh
-if [ -d ${eessi_version}/scripts ]; then
-    # include scripts we wish to ship along with EESSI,
-    find ${eessi_version}/scripts -type f | grep -v '/\.wh\.' > ${files_list}
+# include Lmod cache and configuration file (lmodrc.lua),
+if [ -d ${eessi_version}/software/${os}/${cpu_arch_subdir}/.lmod ]; then
+    # skip whiteout files and backup copies of Lmod cache (spiderT.old.*)
+    find ${eessi_version}/software/${os}/${cpu_arch_subdir}/.lmod -type f | egrep -v '/\.wh\.|spiderT.old' >> ${files_list}
 fi
 
 if [ -d ${eessi_version}/software/${os}/${cpu_arch_subdir}/.lmod ]; then
@@ -51,6 +51,12 @@ if [ -d ${eessi_version}/software/${os}/${cpu_arch_subdir}/.lmod ]; then
     # skip whiteout files and backup copies of Lmod cache (spiderT.old.*)
     find ${eessi_version}/software/${os}/${cpu_arch_subdir}/.lmod -type f | egrep -v '/\.wh\.|spiderT.old' >> ${files_list}
 fi
+
+# include scripts that were copied by install_scripts.sh, which we want to ship in EESSI repository
+if [ -d ${eessi_version}/scripts ]; then
+    find ${eessi_version}/scripts -type f | grep -v '/\.wh\.' >> ${files_list}
+fi
+
 if [ -d ${eessi_version}/software/${os}/${cpu_arch_subdir}/modules ]; then
     # module files
     find ${eessi_version}/software/${os}/${cpu_arch_subdir}/modules -type f | grep -v '/\.wh\.' >> ${files_list}
@@ -61,6 +67,7 @@ if [ -d ${eessi_version}/software/${os}/${cpu_arch_subdir}/modules ]; then
         | grep -v '/\.wh\.' | grep -v '/\.modulerc\.lua' | sed -e 's/.lua$//' | sed -e 's@.*/modules/all/@@g' | sort -u \
         >> ${module_files_list}
 fi
+
 if [ -d ${eessi_version}/software/${os}/${cpu_arch_subdir}/software -a -r ${module_files_list} ]; then
     # installation directories but only those for which module files were created
     # Note, we assume that module names (as defined by 'PACKAGE_NAME/VERSION.lua'
