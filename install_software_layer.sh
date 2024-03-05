499328d70e3c6275eec9dcaf717a34afaf75a6e0
Bob Dröge <b.e.droge@rug.nl>
2024-03-05 15:14:47 +0100

    undo

--- install_software_layer.sh
+++ install_software_layer.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 base_dir=$(dirname $(realpath $0))
 source ${base_dir}/init/eessi_defaults
-if [ $UID -eq 0 ]; then
-    ./EESSI-remove-software.sh "$@"
-    exec runuser -u eessi $( readlink -f "$0" ) -- "$@"
-fi
 ./run_in_compat_layer_env.sh ./EESSI-install-software.sh "$@"
