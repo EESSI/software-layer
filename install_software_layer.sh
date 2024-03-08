4946b085eb6bff9515aa8e2257d3320f778ab6f4
Bob Dröge <b.e.droge@rug.nl>
2024-03-08 10:41:21 +0100

    if run as root, run the removal script

--- install_software_layer.sh
+++ install_software_layer.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 base_dir=$(dirname $(realpath $0))
 source ${base_dir}/init/eessi_defaults
+if [ $EUID -eq 0 ]; then
+    ./EESSI-remove-software.sh "$@"
+    exec runuser -u eessi $( readlink -f "$0" ) -- "$@"
+fi
 ./run_in_compat_layer_env.sh ./EESSI-install-software.sh "$@"
