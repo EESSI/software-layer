db912759e627398bf5db772915f17c26c26ffac9
Thomas Roeblitz <thomas.roblitz@uib.no>
2024-03-26 20:18:03 +0100

    use older apptainer (1.2.4)

--- install_apptainer_ubuntu.sh
+++ install_apptainer_ubuntu.sh
@@ -5,8 +5,11 @@ set -e
 # see https://github.com/apptainer/singularity/issues/5390#issuecomment-899111181
 sudo apt-get install alien
 alien --version
-apptainer_rpm=$(curl --silent -L https://dl.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/a/ | grep 'apptainer-[0-9]' | sed 's/.*\(apptainer[0-9._a-z-]*.rpm\).*/\1/g')
-curl -OL https://dl.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/a/${apptainer_rpm}
+#apptainer_rpm=$(curl --silent -L https://dl.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/a/ | grep 'apptainer-[0-9]' | sed 's/.*\(apptainer[0-9._a-z-]*.rpm\).*/\1/g')
+#curl -OL https://dl.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/a/${apptainer_rpm}
+#/pub/archive/epel/8.8/Everything/x86_64/Packages/a
+apptainer_rpm=$(curl --silent -L https://dl.fedoraproject.org/pub/archive/epel/8.8/Everything/x86_64/Packages/a/ | grep 'apptainer-[0-9]' | sed 's/.*\(apptainer[0-9._a-z-]*.rpm\).*/\1/g')
+curl -OL https://dl.fedoraproject.org/pub/archive/epel/8.8/Everything/x86_64/Packages/a/${apptainer_rpm}
 sudo alien -d ${apptainer_rpm}
 sudo apt install ./apptainer*.deb
 apptainer --version
