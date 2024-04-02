4cf4d76cc80ef5e5aa5df73cef628fca3ad56b99
Caspar van Leeuwen <casparl@tcn1.local.snellius.surf.nl>
2024-04-02 16:03:14 +0200

    Issue is resolved, so we can use Apptainer 1.3.0

--- install_apptainer_ubuntu.sh
+++ install_apptainer_ubuntu.sh
@@ -5,8 +5,6 @@ set -e
 # see https://github.com/apptainer/singularity/issues/5390#issuecomment-899111181
 sudo apt-get install alien
 alien --version
-# Switch back to Apptainer >= 1.3.0 to retrigger
-# https://github.com/EESSI/software-layer/pull/514
 epel_subdir="pub/epel/8"
 apptainer_rpm=$(curl --silent -L https://dl.fedoraproject.org/${epel_subdir}/Everything/x86_64/Packages/a/ | grep 'apptainer-[0-9]' | sed 's/.*\(apptainer[0-9._a-z-]*.rpm\).*/\1/g')
 curl -OL https://dl.fedoraproject.org/${epel_subdir}/Everything/x86_64/Packages/a/${apptainer_rpm}
