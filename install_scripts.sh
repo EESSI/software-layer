5ca2c4a25d702372f0993b57025a7f01459b21d9
Alan O'Cais <alan.ocais@cecam.org>
2024-04-19 09:52:39 +0200

    Copy over hooks along with initi files

--- install_scripts.sh
+++ install_scripts.sh
@@ -113,3 +113,9 @@ nvidia_files=(
     install_cuda_host_injections.sh link_nvidia_host_libraries.sh
 )
 copy_files_by_list ${TOPDIR}/scripts/gpu_support/nvidia ${INSTALL_PREFIX}/scripts/gpu_support/nvidia "${nvidia_files[@]}"
+
+# Copy over EasyBuild hooks file used for installations
+hook_files=(
+    eb_hooks.py
+)
+copy_files_by_list ${TOPDIR} ${INSTALL_PREFIX}/easybuild "${hook_files[@]}"
