33cb74f5e2f4f63cd286e0d1b71818ccb1eca1a7
Bob Dröge <b.e.droge@rug.nl>
2024-04-26 15:33:15 +0200

    try adding write permissions instead of removing

--- EESSI-remove-software.sh
+++ EESSI-remove-software.sh
@@ -112,8 +112,9 @@ if [ $EUID -eq 0 ]; then
                     app_dir=${EASYBUILD_INSTALLPATH}/software/${app}
                     app_module=${EASYBUILD_INSTALLPATH}/modules/all/${app}.lua
                     echo_yellow "Removing ${app_dir} and ${app_module}..."
-                    rm -rf ${app_dir}
-                    rm -rf ${app_module}
+                    #rm -rf ${app_dir}
+                    #rm -rf ${app_module}
+                    chmod -R u+w ${app_dir} ${app_module}
                 done
             else
                 fatal_error "Easystack file ${easystack_file} not found!"
