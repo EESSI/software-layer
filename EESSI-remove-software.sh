66724f282e405579021811813721aaf79dc35a75
Bob Dröge <b.e.droge@rug.nl>
2024-04-30 15:14:50 +0200

    undo changes

--- EESSI-remove-software.sh
+++ EESSI-remove-software.sh
@@ -112,9 +112,8 @@ if [ $EUID -eq 0 ]; then
                     app_dir=${EASYBUILD_INSTALLPATH}/software/${app}
                     app_module=${EASYBUILD_INSTALLPATH}/modules/all/${app}.lua
                     echo_yellow "Removing ${app_dir} and ${app_module}..."
-                    #rm -rf ${app_dir}
+                    rm -rf ${app_dir}
                     rm -rf ${app_module}
-                    chmod -R u+w ${app_dir}
                 done
             else
                 fatal_error "Easystack file ${easystack_file} not found!"
