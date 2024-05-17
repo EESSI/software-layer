0d8a896a20a2e7875b4e6c0551089161142a5d32
Thomas Roeblitz <thomas.roblitz@uib.no>
2024-05-17 12:42:42 +0200

    fix arguments to cuDNN install script

--- EESSI-install-software.sh
+++ EESSI-install-software.sh
@@ -234,7 +234,7 @@ fi
 
 if [ -z "${skip_cuda_install}" ] || [ ! "${skip_cuda_install}" ]; then
     ${EESSI_PREFIX}/scripts/gpu_support/nvidia/install_cuda_host_injections.sh -c 12.1.1 --accept-cuda-eula
-    ${EESSI_PREFIX}/scripts/gpu_support/nvidia/install_cudnn_host_injections.sh -c 12.1.1 --accept-cuda-eula
+    ${EESSI_PREFIX}/scripts/gpu_support/nvidia/install_cudnn_host_injections.sh -c 12.1.1 -d 8.9.2.26
 else
     echo "Skipping installation of CUDA SDK and cu* libraries in host_injections, since the --skip-cuda-install flag was passed OR no EasyBuild module was found"
 fi
