f974c34a85b3fb322906b974b7fcf084fa6398df
Pedro Santos Neves <10762799+Neves-P@users.noreply.github.com>
2024-05-27 09:49:10 +0200

    Fix typo in create_lmodsitepackage.py ESPReSso warning

--- create_lmodsitepackage.py
+++ create_lmodsitepackage.py
@@ -180,7 +180,7 @@ local function eessi_espresso_deprecated_warning(t)
     if simpleName == 'ESPResSo' and version == '4.2.1' then
         local advice = 'Prefer versions  >= 4.2.2 which include important bugfixes.\\n'
         advice = advice .. 'For details see https://github.com/espressomd/espresso/releases/tag/4.2.2\\n'
-        advice = advice .. 'Use version 4.2.1 at your own risk!\\nn'
+        advice = advice .. 'Use version 4.2.1 at your own risk!\\n'
         LmodWarning("\\nESPReSso v4.2.1 has known issues and has been deprecated. ", advice)
     end
 end
