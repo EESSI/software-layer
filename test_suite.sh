a8602734a213f331ccd77718b96970196b4748fa
Caspar van Leeuwen <casparl@tcn1.local.snellius.surf.nl>
2024-08-20 12:20:28 +0200

    Cleanup debugging output

--- test_suite.sh
+++ test_suite.sh
@@ -166,9 +166,6 @@ else
 fi
 cgroup_v1_mem_limit="/hostsys/fs/cgroup/memory/$(</proc/self/cpuset)/memory.limit_in_bytes"
 cgroup_v2_mem_limit="/hostsys/fs/cgroup/$(</proc/self/cpuset)/memory.max"
-echo "cpuset_file: $(</proc/self/cpuset)"
-echo "ls -al /hostsys/fs/cgroup/memory/$(</proc/self/cpuset)"
-ls -al "/hostsys/fs/cgroup/memory/$(</proc/self/cpuset)"
 if [ -f "$cgroup_v1_mem_limit" ]; then
     echo "Getting memory limit from file $cgroup_v1_mem_limit"
     cgroup_mem_bytes=$(cat "$cgroup_v1_mem_limit")
