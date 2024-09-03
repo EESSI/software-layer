238b4074cdb3c8fe6f28681e900f217b25ef0bfe
Caspar van Leeuwen <casparl@int6.local.snellius.surf.nl>
2024-09-03 18:10:00 +0200

    Cleanup of code that is now no longer needed since we use ReFrame's CPU autodetection

--- test_suite.sh
+++ test_suite.sh
@@ -141,34 +141,6 @@ export RFM_PREFIX=$PWD/reframe_runs
 echo "Configured reframe with the following environment variables:"
 env | grep "RFM_"
 
-# Inject correct CPU/memory properties into the ReFrame config file
-echo "Collecting system-specific input for the ReFrame configuration file"
-cpuinfo=$(lscpu)
-if [[ "${cpuinfo}" =~ CPU\(s\):[^0-9]*([0-9]+) ]]; then
-    cpu_count=${BASH_REMATCH[1]}
-    echo "Detected CPU count: ${cpu_count}"
-else
-    fatal_error "Failed to get the number of CPUs for the current test hardware with lscpu."
-fi
-if [[ "${cpuinfo}" =~ Socket\(s\):[^0-9]*([0-9]+) ]]; then
-    socket_count=${BASH_REMATCH[1]}
-    echo "Detected socket count: ${socket_count}"
-else
-    fatal_error "Failed to get the number of sockets for the current test hardware with lscpu."
-fi
-if [[ "${cpuinfo}" =~ (Thread\(s\) per core:[^0-9]*([0-9]+)) ]]; then
-    threads_per_core=${BASH_REMATCH[2]}
-    echo "Detected threads per core: ${threads_per_core}"
-else
-    fatal_error "Failed to get the number of threads per core for the current test hardware with lscpu."
-fi
-if [[ "${cpuinfo}" =~ (Core\(s\) per socket:[^0-9]*([0-9]+)) ]]; then
-    cores_per_socket=${BASH_REMATCH[2]}
-    echo "Detected cores per socket: ${cores_per_socket}"
-else
-    fatal_error "Failed to get the number of cores per socket for the current test hardware with lscpu."
-fi
-
 # The /sys inside the container is not the same as the /sys of the host
 # We want to extract the memory limit from the cgroup on the host (which is typically set by SLURM).
 # Thus, bot/test.sh bind-mounts the host's /sys/fs/cgroup into /hostsys/fs/cgroup
