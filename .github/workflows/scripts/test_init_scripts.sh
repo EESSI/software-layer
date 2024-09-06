7f95d5e3478f6aa16cef41628ad718d069b83b7f
makanu <matti.kaupenjohann@fh-dortmund.de>
2024-09-06 14:58:26 +0000

    Update init scripts path in test script
    from `/init/$shell` to `/init/lmod/$shell`

--- .github/workflows/scripts/test_init_scripts.sh
+++ .github/workflows/scripts/test_init_scripts.sh
@@ -19,19 +19,19 @@ for shell in ${SHELLS[@]}; do
 	echo = | awk 'NF += (OFS = $_) + 100'
 
 	# TEST 1: Source Script and check Module Output
-	assert "$shell -c 'source init/$shell' 2>&1 " "EESSI/$EESSI_VERSION loaded successfully"
+	assert "$shell -c 'source init/lmod/$shell' 2>&1 " "EESSI/$EESSI_VERSION loaded successfully"
 	# TEST 2: Check PS1 Prompt (Previous exported)
 	PROMPT="${shell^^}_PROMPT"
 	assert "echo ${!PROMPT}" "{EESSI $EESSI_VERSION}"
 	# Test 3: Check module overview
-	MODULE_SECTIONS=($($shell -c "source init/$shell 2>/dev/null; module ov 2>&1 | grep -e '---'"))
+	MODULE_SECTIONS=($($shell -c "source init/lmod/$shell 2>/dev/null; module ov 2>&1 | grep -e '---'"))
 	assert "echo ${MODULE_SECTIONS[1]}" "/cvmfs/software.eessi.io/versions/$EESSI_VERSION/software/linux/x86_64/intel/haswell/modules/all"
 	assert "echo ${MODULE_SECTIONS[4]}" "/cvmfs/software.eessi.io/versions/$EESSI_VERSION/init/modules"
 	# Test 4: Load Python module and check version and path
-	command="$shell -c 'source init/$shell 2>/dev/null; module load Python/3.10.8-GCCcore-12.2.0; python --version'"
+	command="$shell -c 'source init/lmod/$shell 2>/dev/null; module load Python/3.10.8-GCCcore-12.2.0; python --version'"
 	expected="Python 3.10.8"
 	assert "$command" "$expected"
-	command="$shell -c 'source init/$shell 2>/dev/null; module load Python/3.10.8-GCCcore-12.2.0; which python'"
+	command="$shell -c 'source init/lmod/$shell 2>/dev/null; module load Python/3.10.8-GCCcore-12.2.0; which python'"
 	expected="/cvmfs/software.eessi.io/versions/$EESSI_VERSION/software/linux/x86_64/intel/haswell/software/Python/3.10.8-GCCcore-12.2.0/bin/python"
 	assert "$command" "$expected"
 done
