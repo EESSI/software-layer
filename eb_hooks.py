eebd184890922cd205ae0c8c2e66892701e1491f
Caspar van Leeuwen <casparl@tcn1.local.snellius.surf.nl>
2024-09-25 15:25:31 +0200

    Should be a string of course

--- eb_hooks.py
+++ eb_hooks.py
@@ -304,7 +304,7 @@ def parse_hook_freeimage_aarch64(ec, *args, **kwargs):
     """
     if ec.name == 'FreeImage' and ec.version in ('3.18.0',):
         if os.getenv('EESSI_CPU_FAMILY') == 'aarch64':
-            if toolchainopts not in ec:
+            if 'toolchainopts' not in ec:
                 ec['toolchainopts'] = {}
             ec['toolchainopts']['pic'] = True
             print_msg("Changed toolchainopts for %s: %s", ec.name, ec['toolchainopts']) 
