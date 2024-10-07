eb41886d46c90b0f7f0b8ad2f34d0fe69577d645
Caspar van Leeuwen <casparl@int4.local.snellius.surf.nl>
2024-10-07 14:31:50 +0200

    Forgot closing quote

--- bot/check-test.sh
+++ bot/check-test.sh
@@ -83,7 +83,7 @@ else
         grep -Pzo "${GP_success_full}" | \
         sed 's/\x00/<br\/>/g' | 
         sed ':a;N;$!ba;s/\n/<br\/>/g' |
-        sed 's/%/\%/g \
+        sed 's/%/\%/g' \
     )
     grep_reframe_result=${grep_reframe_success_full}
     echo "grep_reframe_success_full: ${grep_reframe_success_full}"
