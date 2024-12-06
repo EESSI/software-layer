e4c8b0419c0e0560eace89a597de99fd70b72c7f
Bob Dröge <b.e.droge@rug.nl>
2024-12-06 13:48:50 +0100

    move block of code that sets $STORAGE and $JOB_STORAGE out of if statement

--- bot/test.sh
+++ bot/test.sh
@@ -103,25 +103,25 @@ fi
 RESUME_DIR=$(grep 'Using .* as tmp directory' slurm-${SLURM_JOBID}.out | head -1 | awk '{print $2}')
 
 if [[ -z ${RESUME_DIR} ]]; then
-  echo -n "setting \$STORAGE by replacing any var in '${LOCAL_TMP}' -> "
-  # replace any env variable in ${LOCAL_TMP} with its
-  #   current value (e.g., a value that is local to the job)
-  STORAGE=$(envsubst <<< ${LOCAL_TMP})
-  echo "'${STORAGE}'"
-
-  # make sure ${STORAGE} exists
-  mkdir -p ${STORAGE}
-
-  # make sure the base tmp storage is unique
-  JOB_STORAGE=$(mktemp --directory --tmpdir=${STORAGE} bot_job_tmp_XXX)
-  echo "bot/test.sh: created unique base tmp storage directory at ${JOB_STORAGE}"
-
   RESUME_TGZ=${PWD}/previous_tmp/build_step/$(ls previous_tmp/build_step)
   if [[ -z ${RESUME_TGZ} ]]; then
     echo "bot/test.sh: no information about tmp directory and tarball of build step; --> giving up"
     exit 2
   fi
 fi
+
+echo -n "setting \$STORAGE by replacing any var in '${LOCAL_TMP}' -> "
+# replace any env variable in ${LOCAL_TMP} with its
+#   current value (e.g., a value that is local to the job)
+STORAGE=$(envsubst <<< ${LOCAL_TMP})
+echo "'${STORAGE}'"
+
+# make sure ${STORAGE} exists
+mkdir -p ${STORAGE}
+
+# make sure the base tmp storage is unique
+JOB_STORAGE=$(mktemp --directory --tmpdir=${STORAGE} bot_job_tmp_XXX)
+echo "bot/test.sh: created unique base tmp storage directory at ${JOB_STORAGE}"
 
 # obtain list of modules to be loaded
 LOAD_MODULES=$(cfg_get_value "site_config" "load_modules")
