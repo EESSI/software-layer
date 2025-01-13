cc7593c05a210defc17cc1fd9381951f63ca03fc
lara <lara@localhost.localdomain>
2025-01-13 13:26:45 +0100

    remove setting GPU path

--- test_suite.sh
+++ test_suite.sh
@@ -83,7 +83,6 @@ export EESSI_SOFTWARE_SUBDIR_OVERRIDE=$(source $TOPDIR/init/bash > /dev/null 2>&
 echo "EESSI_SOFTWARE_SUBDIR_OVERRIDE: $EESSI_SOFTWARE_SUBDIR_OVERRIDE"
 
 source $TOPDIR/init/bash
-export MODULEPATH=/cvmfs/software.eessi.io/versions/2023.06/software/linux/x86_64/amd/zen3/accel/nvidia/cc80/modules/all:$MODULEPATH
 
 # We have to ignore the LMOD cache, otherwise the software that is built in the build step cannot be found/loaded
 # Reason is that the LMOD cache is normally only updated on the Stratum 0, once everything is ingested
