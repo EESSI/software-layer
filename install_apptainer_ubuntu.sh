8ae64a348e837a4fa0e34ab53a23f84b3b3c7571
Alan O'Cais <alan.ocais@cecam.org>
2025-01-13 16:17:40 +0100

    Transition CI workflows to Ubuntu 24.04

--- install_apptainer_ubuntu.sh
+++ install_apptainer_ubuntu.sh
@@ -10,6 +10,21 @@ apptainer_rpm=$(curl --silent -L https://dl.fedoraproject.org/${epel_subdir}/Eve
 curl -OL https://dl.fedoraproject.org/${epel_subdir}/Everything/x86_64/Packages/a/${apptainer_rpm}
 sudo alien -d ${apptainer_rpm}
 sudo apt install ./apptainer*.deb
+# No unpriviledged user name spaces in Ubuntu 23.10+
+if [ "$(lsb_release -r | awk '{print $2}')" \ge "23.10" ]; then
+    sudo tee /etc/apparmor.d/apptainer << 'EOF'
+# Permit unprivileged user namespace creation for apptainer starter
+abi <abi/4.0>,
+include <tunables/global>
+profile apptainer /usr/local/libexec/apptainer/bin/starter{,-suid} 
+    flags=(unconfined) {
+  userns,
+  # Site-specific additions and overrides. See local/README for details.
+  include if exists <local/apptainer>
+}
+EOF
+    sudo systemctl reload apparmor
+fi
 apptainer --version
 # also check whether 'singularity' command is still provided by Apptainer installation
 singularity --version
