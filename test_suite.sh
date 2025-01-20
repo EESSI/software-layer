846a40ca807689f054932a1140b4e526409786ef
lara <lara@localhost.localdomain>
2025-01-20 12:20:51 +0100

    add check for EESSI_ACCELERATOR_TARGET variable

--- test_suite.sh
+++ test_suite.sh
@@ -86,6 +86,7 @@ source $TOPDIR/init/bash
 export LMOD_IGNORE_CACHE=1
 
 echo "test_suite.sh: MODULEPATH='${MODULEPATH}'"
+echo "test_suite.sh: EESSI_ACCELERATOR_TARGET='${EESSI_ACCELERATOR_TARGET}'"
 # Load the ReFrame module
 # Currently, we load the default version. Maybe we should somehow make this configurable in the future?
 module load ReFrame
