16c748f87e42fbb4e068c9e36e517fe1f6db537f
Caspar van Leeuwen <casparl@int4.local.snellius.surf.nl>
2025-02-05 16:02:21 +0100

    Check tmpdir early on

--- bot/build.sh
+++ bot/build.sh
@@ -21,6 +21,8 @@
 # stop as soon as something fails
 set -e
 
+echo "DEBUG: TMPDIR=$TMPDIR"
+
 # Make sure we are referring to software-layer as working directory
 software_layer_dir=$(dirname $(dirname $(realpath $0)))
 # source utils.sh and cfg_files.sh
