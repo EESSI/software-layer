555a3d15a8300169e4de8766f60aa3fc4a91bc10
Lara Ramona Peeters <49882639+laraPPr@users.noreply.github.com>
2025-02-26 14:23:21 +0100

    add script for parsing missing installations output

---
+++ .github/workflows/parse_missing-installations-output.py
@@ -0,0 +1,16 @@
+import os
+import re
+
+eb_missing_out = os.environ['eb_missing_out'])
+missing = re.findall("\([A-Z_a-z0-9_\-_\.]*.eb\)", missing)
+missing_cuda = []
+missing_cpu = []
+for ec in missing:
+    if re.search('CUDA', ec):
+        missing_cuda.append(ec)
+    else:
+        missing_cpu.append(ec)
+if len(missing_cpu) != 0:
+    os.environ['missing_cpu'] = missing_cpu
+if len(missing_gpu) !0:
+    os.environ['missing_cpu'] = missing_cpu
