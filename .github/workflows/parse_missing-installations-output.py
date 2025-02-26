41df4465b881cea904834795014d52cfb4b6f269
Lara Ramona Peeters <49882639+laraPPr@users.noreply.github.com>
2025-02-26 14:48:27 +0100

    fix parse script

--- .github/workflows/parse_missing-installations-output.py
+++ .github/workflows/parse_missing-installations-output.py
@@ -1,7 +1,7 @@
 import os
 import re
 
-eb_missing_out = os.environ['eb_missing_out'])
+eb_missing_out = os.environ['eb_missing_out']
 missing = re.findall("\([A-Z_a-z0-9_\-_\.]*.eb\)", eb_missing_out)
 missing_cuda = []
 missing_cpu = []
@@ -11,6 +11,6 @@ for ec in missing:
     else:
         missing_cpu.append(ec)
 if len(missing_cpu) != 0:
-    os.environ['missing_cpu'] = missing_cpu
-if len(missing_gpu) != 0:
-    os.environ['missing_cpu'] = missing_cpu
+    os.environ['MISSING_CPU'] = str(missing_cpu)
+if len(missing_cuda) != 0:
+    os.environ['MISSING_CUDA'] = str(missing_cuda)
