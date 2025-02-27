bbb8ee9c35706b60439ed7597c9869ff02c4f4ec
torri <torri@disroot.org>
2025-02-27 17:28:25 +0100

    needs more refinement but to show the progress for now

--- licenses/update_licenses.py
+++ licenses/update_licenses.py
@@ -2,98 +2,105 @@ import requests
 import argparse
 import json
 import os
+import re
 from datetime import datetime
 
-parser = argparse.ArgumentParser(description='Script to ingest licenses')
-parser.add_argument('--source', help='Source (GitHub, PyPI, CRAN, Repology) or user')
-parser.add_argument('projects', nargs='+', help='List of project names')
-parser.add_argument('--manual', help='Manually provided license', required=False)
-parser.add_argument('--spdx', help='SPDX identifier for the license', required=False)
-args = parser.parse_args()
+url_repo = "https://repos.ecosyste.ms/api/v1/hosts"    
+url_reg = "https://packages.ecosyste.ms/api/v1/registries"
 
-# Retrieve license from various sources
-def github(source):
-    repo = source.removeprefix('github:')
-    url = (
-        "https://api.github.com/repos/{repo}/license".format(repo=repo)
+def ecosystems_list(url): 
+    r = requests.get(url)
+    if r.status_code != 200:
+        return "not found", None, None
+    data = r.json()
+    listing = []
+    for reg in data: 
+        listing.append(reg["name"])
+    return(listing)
+
+def validate_repo_format(value):
+    # Validates the input format <hostname>:<user>/<repository> and ensures the hostname is allowed.
+    pattern = r'^([^:]+):(?:(\w+)/)?([^/]+)/(.+)$'
+    match = re.match(pattern, value)
+    
+    if not match:
+        raise argparse.ArgumentTypeError(
+            f"Invalid format. Use <hostname>:<user>/<repository> or or <hostname>:<group>/<user>/<repo>.")
+    
+    hostname, group, user, repo = match.groups()
+    
+    if hostname not in ecosystems_list(url_repo):
+        raise argparse.ArgumentTypeError(
+            f"Invalid hostname '{hostname}'. Check '--repo help'"
+        )
+    
+    return value  # Return the validated string
+
+def parse_arguments(): 
+    
+    # Positional arguments
+    parser = argparse.ArgumentParser(description='Script to ingest licenses')
+    parser.add_argument('project', nargs='+', help='List of project name')
+    parser.add_argument(
+        '--manual', help='Manually provided license', required=False)
+    parser.add_argument(
+        '--spdx', help='SPDX identifier for the license', required=False)
+    
+    # Now the complicated ones
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('--registry', help='Origin registry. Use "--registry help" to see all available options', metavar='REGISTRY', choices=ecosystems_list(url_reg))
+    group.add_argument('--repo', help='Origin repository. Format: <host>:<user>/<repo>. All available hosts shown with "--repo help"', metavar='REPOSITORY', type=validate_repo_format)
+
+    args = parser.parse_args()
+    return args
+
+# Retrieve license from  ecosyste.ms package API
+def ecosystems_packages(registry, package):
+    print("available registries: ")
+    ecosystems_registries()
+    url = "https://packages.ecosyste.ms/api/v1/registries/{registry}/packages/{package}".format(
+        registry=registry, package=package
     )
-    headers = {
-        "Accept": "application/vnd.github+json",
-        "Authorization": "Bearer {}".format(os.getenv('GITHUB_TOKEN')),
-        "X-GitHub-Api-Version": "2022-11-28",
-    }
-    r = requests.get(url, headers=headers)
-    if r.status_code != 200:
-        return "not found", None, None
-    data = r.json()
-    return data['license']['spdx_id'], 'GitHub', data['license']['url']
-
-def pypi(project):
-    url = "https://pypi.org/pypi/{project}/json".format(project=project)
+    print(url)
     r = requests.get(url)
     if r.status_code != 200:
         return "not found", None, None
     data = r.json()
-    return data['info']['license'], 'PyPI', data['info'].get('project_url')
+    print(data.get('licenses'))
+    return data.get('normalized_licenses', 'not found'), registry 
 
-def cran(project):
-    url = "http://crandb.r-pkg.org/{project}".format(project=project)
+# Retrieve license from ecosyste.ms repo API 
+def ecosystems_repo(repository, source):
+#    hostname, user, repo = re.match(r'^([^:]+):([^/]+)/(.+)$', repository).groups()
+    hostname, group, user, repo = re.match(r'^([^:]+):(?:(\w+)/)?([^/]+)/(.+)$', repository).groups()
+    
+    if group: 
+        url = "https://repos.ecosyste.ms/api/v1/hosts/{hostname}/repositories/{group}%2F{user}%2F{repo}".format(
+        hostname=hostname, group=group, user=user, repo=repo)
+    else: 
+        url = "https://repos.ecosyste.ms/api/v1/hosts/{hostname}/repositories/{user}%2F{repo}".format(
+        hostname=hostname, user=user, repo=repo)
+    print(url)
     r = requests.get(url)
     if r.status_code != 200:
         return "not found", None, None
     data = r.json()
-    return data['License'], 'CRAN', None
-
-def repology(project):
-    url = "https://repology.org/api/v1/project/{project}".format(
-        project=project
-    )
-    r = requests.get(url)
-    if r.status_code != 200:
-        return "not found", None, None
-    data = r.json()
-    return data.get('license', 'not found'), 'Repology', None
-
-def ecosysteDotms_pypi(project):
-    url = "https://packages.ecosyste.ms/api/v1/registries/pypi.org/packages/{project}".format(
-        project=project
-    )
-    r = requests.get(url)
-    if r.status_code != 200:
-        return "not found", None, None
-    data = r.json()
-    return data.get('license', 'not found'), 'Ecosyste.ms (PyPI)', None
-
-def ecosysteDotms_github(source):
-    repo = source.removeprefix('github:')
-    url = "https://repos.ecosyste.ms/api/v1/hosts/GitHub/repositories/{repo}".format(
-        repo=repo
-    )
-    r = requests.get(url)
-    if r.status_code != 200:
-        return "not found", None, None
-    data = r.json()
-    return data.get('license', 'not found'), 'Ecosyste.ms (GitHub)', None
+    return data.get('license', 'not found')
 
 # Main license retrieval function
-def license_info(project):
-    if args.source == 'pypi':
-        lic, source, url = ecosysteDotms_pypi(project)
-    elif "github" in args.source:
-        lic, source, url = ecosysteDotms_github(args.source)
-    elif args.manual:
-        lic = args.manual
-        source = args.source
-        url = None
+def go_fetch(args):
+    if args.registry:
+        lic, source = ecosystems_packages(args.registry, args.project)
+    elif args.repo:
+        lic = ecosystems_repo(args.repo, args.project)
     else:
         lic, source, url = "not found", None, None
-
-    spdx_id = args.spdx if args.spdx else (lic if lic and lic != "not found" else None)
+        spdx_id = args.spdx if args.spdx else (
+        lic if lic and lic != "not found" else None)
 
     info = {
         "license": lic,
-        "source": source,
-        "spdx_id": spdx_id,
+#        "source": source,
         "retrieved_at": datetime.now().isoformat(),
     }
     return info
@@ -111,7 +118,8 @@ def update_json(licenses, project, info):
             "current": info,
             "history": [info],
         }
-        print('Added new license for project {project}'.format(project=project))
+        print('Added new license for project {project}'.format(
+            project=project))
 
     lic_json = json.dumps(licenses, indent=4)
     with open('licenses.json', 'w') as lic_file:
@@ -120,26 +128,43 @@ def update_json(licenses, project, info):
     return licenses
 
 # Create patch output
+
+
 def generate_patch(licenses):
     patch = json.dumps(licenses, indent=4)
     return patch
 
 # Function to save patch to a file
+
+
 def save_patch(patch_content, filename="license_update.patch"):
     with open(filename, 'w') as patch_file:
         patch_file.write(patch_content)
     print("Patch saved to {filename}".format(filename=filename))
 
+
 def main():
+    args = parse_arguments()
+
     if os.path.exists('licenses.json'):
         with open('licenses.json', 'r') as lic_dict:
             licenses = json.loads(lic_dict.read())
     else:
         licenses = {}
 
-    for project in args.projects:
-        info = license_info(project)
-        update_json(licenses, project, info)
+    for project in args.project:
+        # add if not manual, this just for fetching the license!
+        if not args.manual:
+            # we fetchin'
+            info = go_fetch(args)
+            update_json(licenses, project, info)
+        else: 
+            # we inserting it manually
+            info = {
+                "license": args.spdx,
+                "retrieved_at": datetime.now().isoformat(),
+            }
+	
 
     patch = generate_patch(licenses)
     save_patch(patch)
@@ -149,6 +174,6 @@ def main():
 
     print("Patch output:\n{patch}".format(patch=patch))
 
+
 if __name__ == "__main__":
     main()
-
