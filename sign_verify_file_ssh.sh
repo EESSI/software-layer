2be31133d3f7e3d4caf622c8b8e2c14795d3d303
Alan O'Cais <alan.ocais@cecam.org>
2025-03-10 12:04:37 +0100

    Use conservative umask for entire script

--- sign_verify_file_ssh.sh
+++ sign_verify_file_ssh.sh
@@ -61,9 +61,11 @@ if [ ! -f "$FILE_TO_SIGN" ]; then
     exit $FILE_PROBLEM
 fi
 
+# Use a very conservatuve umask throughout this script since we are dealing with sensitive things
+umask 0177 || { echo "Error: Failed to set 0177 umask."; exit $FILE_PROBLEM; }
+
 # Create a restricted temporary directory and ensure cleanup on exit
 TEMP_DIR=$(mktemp -d) || { echo "Error: Failed to create temporary directory."; exit $FILE_PROBLEM; }
-chmod 700 "$TEMP_DIR"
 trap 'rm -rf "$TEMP_DIR"' EXIT
 
 # Converts the SSH private key to OpenSSH format and generates a public key
