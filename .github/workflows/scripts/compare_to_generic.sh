f38d2f624b6641f1f2919aee080d5db05686c58b
Alan O'Cais <alan.ocais@cecam.org>
2025-05-20 15:38:57 +0200

    Add CUDA software check to stack comparison CI

--- .github/workflows/scripts/compare_to_generic.sh
+++ .github/workflows/scripts/compare_to_generic.sh
@@ -24,3 +24,18 @@ source_of_truth_modules="$base_dir/$source_of_truth/$modules_subdir"
 arch_modules="$base_dir/$target_arch/$modules_subdir"
 echo "Comparing $arch_modules to $source_of_truth_modules"
 python3 $script_dir/compare_stacks.py $source_of_truth_modules $arch_modules
+
+# Also compare NVIDIA GPU software stacks
+if [[ -n "$CUDA_COMPUTE_CAPABILITIES" ]]; then
+    read -ra compute_capabilities <<< "$CUDA_COMPUTE_CAPABILITIES"
+    echo "Also comparing CUDA-enabled software stacks (for compute capabilities: ${compute_capabilities[@]})"
+    # Loop over the array
+    for cc in "${compute_capabilities[@]}"; do
+        source_of_truth_modules="$base_dir/$source_of_truth/accel/nvidia/cc80/$modules_subdir"
+        arch_modules="$base_dir/$target_arch/accel/nvidia/$cc/$modules_subdir"
+        echo "Comparing $arch_modules to $source_of_truth_modules"
+        python3 $script_dir/compare_stacks.py $source_of_truth_modules $arch_modules
+    done
+else
+    echo "CUDA_COMPUTE_CAPABILITIES is not set or is empty, not checking NVIDIA software stacks"
+fi
