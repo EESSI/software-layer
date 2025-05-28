99d8f1b73871973344d8df5f1d26738037933193
Thomas Röblitz <thomas.roblitz@uib.no>
2025-05-28 22:26:37 +0200

    different workaround for missing setting in EB5+

--- eb_hooks.py
+++ eb_hooks.py
@@ -133,10 +133,11 @@ def post_ready_hook(self, *args, **kwargs):
     # Check whether we have EasyBuild 4 or 5
     parallel_param = 'parallel'
     if EASYBUILD_VERSION >= '5':
-        # parallel_param = 'max_parallel'  # EasyBlock.set_parallel sets 'parallel'
-        parallel_param = 'parallel'
+        parallel_param = 'max_parallel'
     # get current parallelism setting
     parallel = self.cfg[parallel_param]
+    if parallel == None:
+        return  # self.cfg doesn't contain 'parallel' or 'max_parallel'
     if parallel == 1:
         return  # no need to limit if already using 1 core
 
